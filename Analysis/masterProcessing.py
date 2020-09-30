282ebc898cb4fcbf41fbac6520d0cdc30e779eea
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-09-30 13:05:26 +0200

    Update masterProcessing.py

--- Analysis/masterProcessing.py
+++ Analysis/masterProcessing.py
@@ -6,7 +6,7 @@ Created on Tue Sep 29 15:53:54 2020
 @author: victoronink
 """
 import os
-from FileNaming import parameters
+from FileNaming import parameters,filenames
 #%%
 par=parameters(sce=int(os.environ['SCENARIO']),vic=int(os.environ['VICINITY']),
                st=int(os.environ['SHORETIME']),rt=int(os.environ['RESUSTIME']),
@@ -14,5 +14,6 @@ par=parameters(sce=int(os.environ['SCENARIO']),vic=int(os.environ['VICINITY']),
                inp=int(os.environ['INPUT']),year=int(os.environ['STARTYEAR']),
                stoke=int(os.environ['STOKES']),ens=int(os.environ['ENSEMBLE']),
                simlen=int(os.environ['SIMLEN']))
-os.system('echo The beaching scenario is '+par.scenario)
 par.printScenario()
+
+os.system('echo The beaching scenario is '+filenames.scenario)
