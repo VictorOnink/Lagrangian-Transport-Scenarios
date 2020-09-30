af7d67a0ea71d2a32755ae8e0d6b185a87ae0173
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-09-30 12:30:22 +0200

    Update masterProcessing.py

--- Analysis/masterProcessing.py
+++ Analysis/masterProcessing.py
@@ -8,7 +8,6 @@ Created on Tue Sep 29 15:53:54 2020
 import os
 from FileNaming import parameters
 #%%
-os.system('echo '+str(globals()))
 os.system('echo '+str(int(os.environ['SCENARIO'])))
-os.system('echo The beaching scenario is '+parameters.scenario)
+os.system('echo The beaching scenario is '+parameters.input)
 parameters.printScenario()
