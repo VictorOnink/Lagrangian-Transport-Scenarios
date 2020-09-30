56ede5bb353ec50ad8a9215417319587b4f9d087
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-09-30 14:39:20 +0200

    simplified

--- Analysis/FileNaming.py
+++ Analysis/FileNaming.py
@@ -9,10 +9,55 @@ This will be a class that I will use for all my file naming
 """
 import os
 
-class Parameters(object):
-    """
-    This class is for storing all the different run parameters 
-    """
+# class Parameters(object):
+#     """
+#     This class is for storing all the different run parameters 
+#     """
+#     def __init__(self,sce,vic,st,rt,sd,Wmin,inp,year,stoke,ens,simlen):
+#         #Naming the scenario
+#         scenarionames = {0:'AdvectionDiffusionOnly',1:'CoastalProximity',2:'Stochastic',
+#                          3:'ShoreDependentResuspension',4:'TurrellResuspension'}
+#         self.scenario = scenarionames[sce]
+#         #Beaching scenario specific parameters
+#         if self.scenario == 'CoastalProximity':
+#             #Time near coast before beaching
+#             self.vicinity = vic
+#         if self.scenario in ['Stochastic','ShoreDependentResuspension']:
+#             #Beaching e-folding timescale (days)
+#             self.shoretime = st
+#             #Resuspension e-folding timescale (days)
+#             self.resustime = rt
+#         if self.scenario == 'ShoreDependentResuspension':
+#             #Resuspension shore dependence scenario
+#             self.shoreDepen = sd
+#         if self.scenario == 'TurrellResuspension':
+#             #Beaching e-folding timescale (days)
+#             self.shoretime = st
+#             #Resuspension windspeed (m/s)
+#             self.Wmin = Wmin
+#         #Input scenario specific parameters
+#         inputnames={0:'Jambeck',1:'Lebreton'}
+#         self.input = inputnames[inp]
+#         #Number of runs in each input scenario
+#         if self.input == 'Jambeck':
+#             self.run = 8
+#         elif self.input == 'Lebreton':
+#             self.run = 3
+#         #Parameters relevant for all scenarios
+#         #Starting year of the simulation
+#         self.startyear = year
+#         #0 = Stokes included, 1 = Stokes not included
+#         self.stokes = stoke
+#         #Ensemble number
+#         self.ensemble = ens
+#         #Length of the simulation (years)
+#         self.simlen = simlen
+    
+   
+#     def printScenario(self):
+#         os.system('echo The beaching scenario is '+self.scenario)
+        
+class FileNames(object):
     def __init__(self,sce,vic,st,rt,sd,Wmin,inp,year,stoke,ens,simlen):
         #Naming the scenario
         scenarionames = {0:'AdvectionDiffusionOnly',1:'CoastalProximity',2:'Stochastic',
@@ -52,14 +97,6 @@ class Parameters(object):
         self.ensemble = ens
         #Length of the simulation (years)
         self.simlen = simlen
-    
-   
-    def printScenario(self):
-        os.system('echo The beaching scenario is '+self.input)
-        
-class FileNames(object):
-    def __init__(self,par):
-        par.__init__(self)
         
     def printScenario(self):
         os.system('echo The beaching scenario is '+self.scenario)
--- Analysis/masterProcessing.py
+++ Analysis/masterProcessing.py
@@ -6,22 +6,13 @@ Created on Tue Sep 29 15:53:54 2020
 @author: victoronink
 """
 import os
-from FileNaming import Parameters
 from FileNaming import FileNames
 #%%
-par=Parameters(sce=int(os.environ['SCENARIO']),vic=int(os.environ['VICINITY']),
-               st=int(os.environ['SHORETIME']),rt=int(os.environ['RESUSTIME']),
-               sd=int(os.environ['SHOREDEPEN']),Wmin=int(os.environ['WMIN']),
-               inp=int(os.environ['INPUT']),year=int(os.environ['STARTYEAR']),
-               stoke=int(os.environ['STOKES']),ens=int(os.environ['ENSEMBLE']),
-               simlen=int(os.environ['SIMLEN']))
-os.system('echo The beaching scenario is '+par.scenario)
-par.printScenario()
-
-fn=FileNames(Parameters(sce=int(os.environ['SCENARIO']),vic=int(os.environ['VICINITY']),
-               st=int(os.environ['SHORETIME']),rt=int(os.environ['RESUSTIME']),
-               sd=int(os.environ['SHOREDEPEN']),Wmin=int(os.environ['WMIN']),
-               inp=int(os.environ['INPUT']),year=int(os.environ['STARTYEAR']),
-               stoke=int(os.environ['STOKES']),ens=int(os.environ['ENSEMBLE']),
-               simlen=int(os.environ['SIMLEN'])))
+fn=FileNames(sce=int(os.environ['SCENARIO']),vic=int(os.environ['VICINITY']),
+             st=int(os.environ['SHORETIME']),rt=int(os.environ['RESUSTIME']),
+             sd=int(os.environ['SHOREDEPEN']),Wmin=int(os.environ['WMIN']),
+             inp=int(os.environ['INPUT']),year=int(os.environ['STARTYEAR']),
+             stoke=int(os.environ['STOKES']),ens=int(os.environ['ENSEMBLE']),
+             simlen=int(os.environ['SIMLEN']))
+os.system('echo The beaching scenario is '+fn.scenario)
 fn.printScenario()
