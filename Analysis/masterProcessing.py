b571817a6abb8f8a82c51b0439bf4fa3c9be6514
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-09-30 12:16:12 +0200

    Update masterProcessing.py

--- Analysis/masterProcessing.py
+++ Analysis/masterProcessing.py
@@ -5,7 +5,8 @@ Created on Tue Sep 29 15:53:54 2020
 
 @author: victoronink
 """
-
+import os
 from FileNaming import parameters
 #%%
+os.system('echo The beaching scenario is '+parameters.scenario)
 parameters.printScenario()
