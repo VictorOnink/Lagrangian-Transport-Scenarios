85e3fd02090e165811161f038ad529f9fef6ebed
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-10-06 13:32:57 +0200

    try again

--- src/scenarios/base_scenario.py
+++ src/scenarios/base_scenario.py
@@ -48,19 +48,18 @@ class BaseScenario(ABC):
         pass
 
     @abstractmethod
-    def _get_var_dict(self) -> dict:
-        if settings.RESTART == 0:
-            return pvf.initialize_variable_dict_from_varlist(self.var_list)
-        else:
-            return self._get_restart_variables(rfile=self.self._file_names(new=False), var_list=self.var_list)
-
-    @abstractmethod
     def _beaching_kernel(self) -> ParticleSet:
         pass
 
     @abstractmethod
     def _get_particle_behavior(self):
         pass
+
+    def _get_var_dict(self) -> dict:
+        if settings.RESTART == 0:
+            return pvf.initialize_variable_dict_from_varlist(self.var_list)
+        else:
+            return self._get_restart_variables(rfile=self.self._file_names(new=False), var_list=self.var_list)
 
     def run(self) -> object:
         pset = self._get_pset(fieldset=self.field_set, particle_type=self.particle,
