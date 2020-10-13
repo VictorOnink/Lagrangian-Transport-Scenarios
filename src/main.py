a36eed1ff35388b07c0b7572cad6db26fc91266e
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-10-13 09:40:34 +0200

    still not working...

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 import settings as set
 import factories.scenario_factory as scenario_factory
 import factories.analysis_factory as analysis_factory
+import os
 
 
 def run():
     if set.SUBMISSION == 'simulation':
+        os.system('echo "Running the simulation"')
         scenario_name = set.SCENARIO_NAME
         stokes = set.STOKES
         server = set.SERVER
         scenario = scenario_factory.ScenarioFactory.create_scenario(scenario_name=scenario_name, stokes=stokes, server=server)
         scenario.run()
     elif set.SUBMISSION == 'analysis':
+        os.system('echo "Running the analysis"')
         scenario_name = set.SCENARIO_NAME
         stokes = set.STOKES
         server = set.SERVER
