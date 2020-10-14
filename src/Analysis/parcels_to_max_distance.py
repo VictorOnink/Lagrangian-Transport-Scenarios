ba8ef0203ba9c25ea902e40b99038a5464b2c21f
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-10-14 17:23:56 +0200

    try again

--- src/Analysis/parcels_to_max_distance.py
+++ src/Analysis/parcels_to_max_distance.py
@@ -30,10 +30,12 @@ def parcels_to_max_distance(file_dict: dict, lon_min: float = -180, lon_max: flo
                 within_domain = utils._particles_in_domain(domain=domain, lon=lon[:, :1], lat=lat[:, :1])
             # Making a cumulative array for the run
             if restart == 0:
-                max_distance_run = max_distance_file[within_domain]
+                max_distance_file = max_distance_file[within_domain]
+                max_distance_run = max_distance_file.reshape((len(max_distance_file), 1))
                 os.system('echo "shape of max_distance_run"'+str(max_distance_run.shape))
             else:
-                max_distance_run = np.vstack((max_distance_run, max_distance_file[within_domain]))
+                max_distance_file = max_distance_file[within_domain]
+                max_distance_run = np.vstack((max_distance_run, max_distance_file.reshape((len(max_distance_file), 1))))
                 os.system('echo "shape of max_distance_run"' + str(max_distance_run.shape))
         if run == 0:
             max_distance_simulation = np.nanmax(max_distance_run, axis=0, keepdims=True)
