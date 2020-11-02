a38cdcb975dc98628d763caefc316782f6de3b51
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2020-11-02 14:03:51 +0100

    rise velocity tests

--- src/scenarios/FragmentationCozar.py
+++ src/scenarios/FragmentationCozar.py
@@ -152,12 +152,12 @@ class FragmentationCozar(base_scenario.BaseScenario):
             a_del_rho = delta_rho * -1.
             vs = -1. * (g * kin_visc * w * a_del_rho) ** (1. / 3.)  # m s-1
 
-        z0 = z + vs * particle.dt
-        # 1.472102
-        if z0 <= 1.472102 or z0 >= fieldset.bathymetry[time, particle.depth, particle.lat, particle.lon]:
-            # particle.depth = 1.472102
-            vs = 0
-            # particle.depth = z0
+        # z0 = z + vs * particle.dt
+        # # 1.472102
+        # if z0 <= 1.472102 or z0 >= fieldset.bathymetry[time, particle.depth, particle.lat, particle.lon]:
+        #     # particle.depth = 1.472102
+        #     vs = 0
+        #     # particle.depth = z0
 
         particle.rise_velocity = vs
 
