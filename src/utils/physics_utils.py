d11ea6cdd845c38c31398c8bfd1c9456adec8300
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-02-19 10:07:08 +0100

    bug fix

--- src/utils/physics_utils.py
+++ src/utils/physics_utils.py
@@ -399,10 +399,11 @@ def KPP_wind_mixing(particle, fieldset, time):
         z_correct = math.fabs(z_correct + 0.5 * dKz * particle.dt)
         alpha = (fieldset.VK * U_W) / fieldset.PHI
         Kz = alpha * (z_correct + z0) * math.pow(1 - z_correct / mld, 2) + fieldset.K_Z_BULK
-        particle.check = Kz
+
 
     # The Markov-0 vertical transport following Ross & Sharples (2004)
     gradient = dKz * particle.dt
+    particle.check = gradient
     R = ParcelsRandom.uniform(-1., 1.) * math.sqrt(math.fabs(particle.dt) * 3) * math.sqrt(2 * Kz)
     rise = particle.rise_velocity * particle.dt
 
