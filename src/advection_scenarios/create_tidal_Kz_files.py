17528de84058354f9b0a7f6e5f27f8ec3ab28903
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-02-23 10:54:45 +0100

    To ubelix

--- src/advection_scenarios/create_tidal_Kz_files.py
+++ src/advection_scenarios/create_tidal_Kz_files.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import array
+import scipy.interpolate as interpolate
 import xarray
 import progressbar
 from copy import deepcopy
@@ -8,7 +9,7 @@ from netCDF4 import Dataset
 import settings
 import utils
 
-def create_tidal_Kz_files(LON: array, LAT: array, DEPTH: array, BATH_filenames: list, BATH_variables: dict):
+def create_tidal_Kz_files(LON: array, LAT: array, DEPTH: array, BATH_filenames: str, BATH_variables: dict):
     """
     Determining Kz from the estimates of production of turbulent kinetic energy, as outlined in
     de Lavergne et al. (2020) and based on code shared by Clement Vic
@@ -19,4 +20,29 @@ def create_tidal_Kz_files(LON: array, LAT: array, DEPTH: array, BATH_filenames: 
     for key in Dataset(TIDAL_filename).variables.keys():
         TIDAL_data[key] = Dataset(TIDAL_filename).variables[key][:]
 
+    # Load the bathymetry data
+    bathymetry = Dataset(BATH_filenames).variables[BATH_variables['DEPTH']][:]
+    bathymetry[bathymetry.mask] = 0
+
+    # A number of physical parameters
+    gamma = 0.2  # Mixing efficiency
+
+    # The TIDAL_data gridding isn't regular in the z-direction. We will first interpolate the epsilon_tid and
+    # buoyancy_frequency_squared fields onto the DEPTH levels
+    TIDAL_inter = interpolate_to_DEPTH(TIDAL_data, DEPTH)
+
     TIDAL_data[10000]
+
+
+def interpolate_to_DEPTH(TIDAL_data, DEPTH):
+    depth_midpoint = TIDAL_data['depth_midpoint']
+    TIDAL_inter = {}
+    for key in ['buoyancy_frequency_squared', 'epsilon_tid']:
+        field = TIDAL_data[key]
+        field_inter = np.zeros(field.shape)
+        for lat in field.shape[1]:
+            for lon in field.shape[2]:
+                inter_f = interpolate.interp1d(depth_midpoint[:, lat, lon], field[:, lat, lon], bounds_error=False)
+                field_inter[:, lat, lon] = inter_f(DEPTH)
+        TIDAL_inter[key] = field_inter
+    return TIDAL_inter
