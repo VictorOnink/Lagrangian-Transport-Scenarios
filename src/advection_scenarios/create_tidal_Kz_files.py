4c62ce450fe1483630304c236781f2cb925208fe
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-02-23 09:51:15 +0100

    To ubelix

--- src/advection_scenarios/create_tidal_Kz_files.py
+++ src/advection_scenarios/create_tidal_Kz_files.py
@@ -16,7 +16,7 @@ def create_tidal_Kz_files(LON: array, LAT: array, DEPTH: array, BATH_filenames: 
     # Loading the global data from de Lavergne et al. (2020)
     TIDAL_filename = utils._get_input_directory(server=settings.SERVER) + 'global_tidal_energy_dissipation.nc'
     TIDAL_data = {}
-    for key in Dataset(TIDAL_filename).variables.keys:
+    for key in Dataset(TIDAL_filename).variables.keys():
         TIDAL_data[key] = Dataset(TIDAL_filename).variables[key][:]
 
     TIDAL_data[10000]
