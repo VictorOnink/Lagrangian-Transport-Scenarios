b92b852b8c37c237d1560631227f25c241333dec
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-14 15:55:35 +0200

    update

--- bin/simulation_submission.sh
+++ bin/simulation_submission.sh
@@ -9,7 +9,7 @@ SCENARIO=6
 #for scenario 1, the time a particle must be near the coast to beach (in days)
 VICINITY=2
 #for scenario 2, the beaching and resuspension timescales (in days)
-SHORETIME=10
+SHORETIME=20
 RESUSTIME=69
 #for scenario 3, the shore dependence scenario.
 SHOREDEPEN=0
