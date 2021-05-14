2fddd1d8d530a8c0ba6b993102209ade9054f38e
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-14 16:00:40 +0200

    update

--- src/utils/physics_utils.py
+++ src/utils/physics_utils.py
@@ -479,6 +479,6 @@ def get_resuspension_timescale(L=settings.INIT_SIZE):
     w_rise = initial_estimate_particle_rise_velocity(L=L)
     lambda_R = 2.6e2 * w_rise + 7.1
     os.system(
-        'echo "The resuspension timescale for a particle of size {} is {:.6f} days"'.format(settings.PARTICLE_SIZE,
+        'echo "The resuspension timescale for a particle of size {} is {:.6f} days"'.format(settings.INIT_SIZE,
                                                                                             lambda_R))
     return lambda_R
