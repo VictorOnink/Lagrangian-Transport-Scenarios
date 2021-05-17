edf0ad135219f8038314da7782ec6114716196be
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-17 13:25:00 +0200

    update

--- bin/analysis_submission.sh
+++ bin/analysis_submission.sh
@@ -5,23 +5,36 @@
 #####################################################################################
 SUBMISSION='analysis'
 export SUBMISSION
-#0=first order, 1=coastal, 2=stochastic beaching/resuspension, 3=coast type dependent, 4=Turrell
-SCENARIO=4
+DEBUG=0 # 0 = Not a debug run, 1 = a debug run
+#0=first order, 1=coastal, 2=stochastic beaching/resuspension, 3=coast type dependent, 4 = Turrell (2020)
+#5 = Cozar based fragmentation, 6 = Size dependent transport
+SCENARIO=6
 #for scenario 1, the time a particle must be near the coast to beach (in days)
 VICINITY=2
 #for scenario 2, the beaching and resuspension timescales (in days)
-SHORETIME=10
+SHORETIME=20
 RESUSTIME=69
-#For scenario 3, we need to indicate if beaching is more likely with sand or not sand. 0 = more sand is less likely beaching and resuspension, 1 = more sand is more likely beaching and resuspension
+#for scenario 3, the shore dependence scenario.
 SHOREDEPEN=0
 #for scenario 4, the minimum wind speed for resusplension. Divide by 10 for actual value
 WMIN=3
+#for scenario 6, the initial size of the particle in 1e-5 m
+PARTICLE_SIZE=1
 #the starting year of the simulation, and how many years the simulation will take
 STARTYEAR=2010
-SIMLEN=1 #Number of years the simulation runs
-STOKES=0 #0 = include stokes, 1 = do not include stokes
-INPUT=0 #0=Jambeck, higher values aren't relevant yet but can be added
-ENSEMBLE=1 #For when we want to run multiple iterations of the same parameter setup
+#Which input distribution do we want to use? 0=Jambeck, 1=lebreton, 2=point release, 3=uniform release
+INPUT=1
+#Which advection data do we want to use?
+# 0 = Global HYCOM, 1 = Caribbean HYCOM, 2 = Mediterranean CMEMS
+ADVECTION_DATA=2
+#Number of years the simulation runs
+SIMLEN=1
+#Inclusion of Stokes drift. 0 = include stokes, 1 = do not include stokes
+STOKES=0
+#Ensemble member
+ENSEMBLE=1
+#Ubelix server, so server==1
+SERVER=1
 
 export SCENARIO
 export VICINITY
@@ -29,24 +42,26 @@ export SHORETIME
 export RESUSTIME
 export SHOREDEPEN
 export WMIN
+export PARTICLE_SIZE
 export SIMLEN
 export STOKES
 export INPUT
 export ENSEMBLE
 export STARTYEAR
+export ADVECTION_DATA
+export SERVER
 
 #A number of switches to indicate which analysis steps we want to run.
 #0 = off, 1 = on
-CONCENTRATION=0
+CONCENTRATION=1
 TIMESERIES=0
-MAX_DISTANCE=1
+MAX_DISTANCE=0
 
 export CONCENTRATION
 export TIMESERIES
 export MAX_DISTANCE
 
 #Now, we can set the job name prefix
-
 if [ "$SCENARIO" -eq "0" ]; then
 	RUNNAMEPREFIX="Analysis_AdvDifOnly_y="${STARTYEAR}"_"
 	if [ "$STOKES" -eq "1" ]; then
@@ -63,12 +78,22 @@ elif [ "$SCENARIO" -eq "2" ]; then
 	    RUNNAMEPREFIX=${RUNNAMEPREFIX}"NS_"
         fi
 elif [ "$SCENARIO" -eq "3" ]; then
-        RUNNAMEPREFIX="Analysis_SDResus_SD="${shoreDepen}"_ST="${SHORETIME}"_RT="${RESUSTIME}"_y"${STARTYEAR}"_"
+        RUNNAMEPREFIX="Analysis_SDResus_SD="${SHOREDEPEN}"_ST="${SHORETIME}"_RT="${RESUSTIME}"_y"${STARTYEAR}"_"
         if [ "$STOKES" -eq "1" ]; then
 	    RUNNAMEPREFIX=${RUNNAMEPREFIX}"NS_"
         fi
 elif [ "$SCENARIO" -eq "4" ]; then
         RUNNAMEPREFIX="Analysis_Turrell_Wmin="${WMIN}"_ST="${SHORETIME}"_y"${STARTYEAR}"_"
+        if [ "$STOKES" -eq "1" ]; then
+	    RUNNAMEPREFIX=${RUNNAMEPREFIX}"NS_"
+        fi
+elif [ "$SCENARIO" -eq "5" ]; then
+        RUNNAMEPREFIX="Analysis_KaandorpFrag_ST="${SHORETIME}"_RT="${RESUSTIME}"_y"${STARTYEAR}"_"
+        if [ "$STOKES" -eq "1" ]; then
+	    RUNNAMEPREFIX=${RUNNAMEPREFIX}"NS_"
+        fi
+elif [ "$SCENARIO" -eq "6" ]; then
+        RUNNAMEPREFIX="Analysis_SizeTransport_SIZE="${PARTICLE_SIZE}"_ST="${SHORETIME}"_RT="${RESUSTIME}"_y"${STARTYEAR}"_"
         if [ "$STOKES" -eq "1" ]; then
 	    RUNNAMEPREFIX=${RUNNAMEPREFIX}"NS_"
         fi
@@ -87,14 +112,21 @@ part3="#SBATCH --mail-user=victor.onink@climate.unibe.ch"
 part4="#SBATCH --job-name="$runname
 part5="#SBATCH --output="runOutput/$runname".o%j"m
 part6="#SBATCH --mem-per-cpu=6G"
-part7="#SBATCH --time=00:10:00"
-part8='#SBATCH --partition=debug'
+if [ "$DEBUG" -eq "0" ]; then
+      part7="#SBATCH --time=95:59:00"
+      part8="#SBATCH --partition=epyc2"
+      part9='#SBATCH --qos=job_epyc2'
+else
+      part7="#SBATCH --time=00:10:00"
+      part8="#SBATCH --partition=epyc2"
+      part9='#SBATCH --qos=job_epyc2_debug'
+fi
 #loading the bash and setting the environment
-part9="source /home/ubelix/climate/vo18e689/.bash_profile"
-part10="source /home/ubelix/climate/vo18e689/anaconda3/bin/activate py3_parcels_v2_2"
-part11='cd "/home/ubelix/climate/vo18e689/codes/Next-Stage-Plastic-Beaching/"'
+part10="source /storage/homefs/vo18e689/.bash_profile"
+part11="source /storage/homefs/vo18e689/anaconda3/bin/activate py3_parcels"
+part12='cd "/storage/homefs/vo18e689/codes/Next-Stage-Plastic-Beaching/"'
 #And now the actual running of the code
-part12="python src/main.py -p 10 -v"
+part13="python src/main.py -p 10 -v"
 #and now the creation of the submission file
 for i in {1..12} 
 do
--- src/Analysis/parcels_to_concentration.py
+++ src/Analysis/parcels_to_concentration.py
@@ -1,6 +1,6 @@
 import settings as settings
 import utils
-
+from advection_scenarios import advection_files
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import numpy as np
@@ -10,21 +10,26 @@ import os
 
 
 def parcels_to_concentration(file_dict: dict):
-    dataset = Dataset(settings.DATA_DIR_SERVERS[settings.SERVER] + 'HYCOM/HYCOM_Surface_3h_2000-01-01.nc')
-    uo = dataset.variables['water_u'][0, 0, :, :]
-    # Load HYCOM latitude, longitude and set a zero array with the same grid size as
-    lat_hyc = dataset.variables['lat'][:]
-    lon_hyc = dataset.variables['lon'][:]
-    overall_concentration = np.zeros(uo.shape)
     # Generate the grid for the hexbin operation
+    advection_scenario = advection_files.AdvectionFiles(server=settings.SERVER, stokes=settings.STOKES,
+                                                        advection_scenario=settings.ADVECTION_DATA,
+                                                        repeat_dt=None)
+    adv_file_dict = advection_scenario.file_names
     bin_number = 1000
-    lon_min, lon_max = -180, 180
-    lat_min, lat_max = -90, 90
+    LON, LAT, GRID = adv_file_dict['LON'], adv_file_dict['LAT'], adv_file_dict['GRID']
+    lon_min, lon_max = np.nanmin(LON), np.nanmax(LON)
+    lat_min, lat_max = np.nanmin(LAT), np.nanmax(LAT)
     hex_grid = Hexagonal2DGrid((bin_number, bin_number), [lon_min, lon_max, lat_min, lat_max])
+    # Getting the grid for the final concentrations
 
     output_direc = utils.get_output_directory(server=settings.SERVER) + 'concentrations/'
     # Counter for the number of files
     counter = 0
+    # Create the output dictionary
+    output_dict = {'overall_concentration': np.zeros(GRID.shape), 'lon': LON, 'lat': LAT}
+    for simulation_years in range(settings.SIM_LENGTH):
+        output_dict[simulation_years] = np.zeros(GRID.shape)
+
     # loop through the runs
     for run in progressbar.progressbar(range(settings.RUN_RANGE)):
         # Loop through the restart files
@@ -33,23 +38,35 @@ def parcels_to_concentration(file_dict: dict):
             parcels_file = file_dict[run][restart]
             dataset = Dataset(parcels_file)
             lon, lat = dataset.variables['lon'][:, :-1], dataset.variables['lat'][:, :-1]
-            weight = dataset.variables['weights'][:, :-1] * settings.BUOYANT
+            if 'weights' in dataset.variables.keys():
+                weight = dataset.variables['weights'][:, :-1] * settings.BUOYANT
+            else:
+                weight = np.ones(lon.shape,dtype=np.float32)
             # Flatten the arrays
             lon_f, lat_f, weight_f = lon.flatten(), lat.flatten(), weight.flatten()
             # Carry out the hex bin operation
             hexagon_cumulative_sum, hexagon_coord = hexbin(lon_f, lat_f, weight_f, hex_grid)
             # Get the average per day, so divide by the number of days in the year
             hexagon_cumulative_sum /= lon.shape[1]
-            # Get the concentration onto the HYCOM grid
-            overall_concentration += utils.histogram(lon_data=hexagon_coord[:, 0], lat_data=hexagon_coord[:, 1],
-                                                     bins_Lon=lon_hyc, bins_Lat=lat_hyc,
-                                                     weight_data=hexagon_cumulative_sum
-                                                     )
+            # Get the concentration onto the advection grid
+            output_dict['overall_concentration'] += utils.histogram(lon_data=hexagon_coord[:, 0],
+                                                                    lat_data=hexagon_coord[:, 1],
+                                                                    bins_Lon=LON, bins_Lat=LAT,
+                                                                    weight_data=hexagon_cumulative_sum
+                                                                    )
             counter += 1
+            # Getting the concentration for the last particle positions with a simulation year
+            hexagon_last, hexagon_coord = hexbin(lon[:, -1], lat[:, -1], weight[:, -1], hex_grid)
+            output_dict[restart] += utils.histogram(lon_data=hexagon_coord[:, 0], lat_data=hexagon_coord[:, 1],
+                                                    bins_Lon=LON, bins_Lat=LAT, weight_data=hexagon_last)
+
     # Divide by the number of files we are averaging over
-    overall_concentration /= counter
-    # Create the output dictionary
-    output_dict = {'concentration': overall_concentration, 'lon': lon_hyc, 'lat': lat_hyc}
+    output_dict['overall_concentration'] /= counter
+
+    # Dividing the end of year concentrations by the number of runs
+    for restart in range(settings.SIM_LENGTH):
+        output_dict[restart] /= settings.RUN_RANGE
+
     # Saving the computed concentration
     prefix = 'concentration'
     output_name = output_direc + utils._analysis_save_file_name(input_file=file_dict[0][0], prefix=prefix)
