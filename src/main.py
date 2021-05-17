4c08d82a123cd01dadf49f1f69e2f4211804f1e8
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-17 13:41:57 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,28 +1,29 @@
-import settings as set
+import os
+os.system('echo "is this working"')
+import settings
 import factories.scenario_factory as scenario_factory
 import factories.analysis_factory as analysis_factory
-import os
 
 
 def run():
-    if set.SUBMISSION == 'simulation':
+    if settings.SUBMISSION == 'simulation':
         os.system('echo "Running the simulation"')
-        scenario_name = set.SCENARIO_NAME
-        stokes = set.STOKES
-        server = set.SERVER
+        scenario_name = settings.SCENARIO_NAME
+        stokes = settings.STOKES
+        server = settings.SERVER
         scenario = scenario_factory.ScenarioFactory.create_scenario(scenario_name=scenario_name, stokes=stokes,
                                                                     server=server)
         scenario.run()
-    elif set.SUBMISSION == 'analysis':
+    elif settings.SUBMISSION == 'analysis':
         os.system('echo "Running the analysis"')
-        scenario_name = set.SCENARIO_NAME
-        stokes = set.STOKES
-        server = set.SERVER
+        scenario_name = settings.SCENARIO_NAME
+        stokes = settings.STOKES
+        server = settings.SERVER
         scenario = scenario_factory.ScenarioFactory.create_scenario(scenario_name=scenario_name, stokes=stokes,
                                                                     server=server)
         file_dict = scenario.return_full_run_directory()
-        analysis_factory.AnalysisFactory.create_procedure(file_dict=file_dict, concentration=set.CONCENTRATION,
-                                                          timeseries=set.TIMESERIES, max_distance=set.MAX_DISTANCE)
+        analysis_factory.AnalysisFactory.create_procedure(file_dict=file_dict, concentration=settings.CONCENTRATION,
+                                                          timeseries=settings.TIMESERIES, max_distance=settings.MAX_DISTANCE)
 
 
 if __name__ == "__main__":
