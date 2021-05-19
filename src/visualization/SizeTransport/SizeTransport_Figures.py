2632b28c2f79efbbf2b1c03f9ff8c69da7ec8c53
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-19 16:01:51 +0200

    update

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -10,7 +10,7 @@ def run(scenario, figure_direc: str):
     :return:
     """
     os.system('echo "start to get the output name"')
-    file_name = scenario._file_names(new=True, advection_data='CMEMS_MEDITERRANEAN', shore_time=20, init_size=500e-5,
+    file_name = scenario._file_names(new=False, advection_data='CMEMS_MEDITERRANEAN', shore_time=20, init_size=settings.INIT_SIZE,
                                      init_density=920, start_year=2010, input='Lebreton', run=settings.RUN,
                                      restart=settings.RESTART)
     os.system('everything seems to be working, file is {}'.format(file_name))
