c7e4eab4ce40eb0cd65cdd90bdc4fa21cde4adae
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-19 17:29:06 +0200

    starting to build in some basic visualization functions, and getting started on the animation function for size dependent transport

--- src/visualization/SizeTransport/SizeTransport_Animation.py
+++ src/visualization/SizeTransport/SizeTransport_Animation.py
@@ -1,6 +1,34 @@
 import settings
 import utils
 import visualization.visualization_utils as vUtils
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+from advection_scenarios import advection_files
+import numpy as np
 
-def SizeTransport_Animation():
-    pass
+
+def SizeTransport_Animation(figure_direc, figsize=(14, 10)):
+    """
+    Here we want to make an animation of the
+    :return:
+    """
+    # Getting the size of the domain that we want to plot for
+    advection_scenario = advection_files.AdvectionFiles(server=settings.SERVER, stokes=settings.STOKES,
+                                                        advection_scenario='CMEMS_MEDITERRANEAN',
+                                                        repeat_dt=None)
+    adv_file_dict = advection_scenario.file_names
+
+    spatial_domain = np.nanmin(adv_file_dict['LON']),  np.nanmax(adv_file_dict['LON']), \
+                     np.nanmin(adv_file_dict['LAT']), np.nanmax(adv_file_dict['LAT'])
+
+    # Setting the folder within which we have the output
+    output_direc = figure_direc + 'animations/'
+    # Creating the base figure
+    fig = plt.figure(figsize=figsize)
+    gs = fig.add_gridspec(1, 1)
+
+    ax = vUtils.cartopy_standard_map(fig=fig, domain=spatial_domain)
+
+    # Setting the output name of the animation, and saving the output
+    output_name = output_direc + 'test.jpg'
+    plt.savefig(output_name)
--- src/visualization/visualization_utils/utils_visual.py
+++ src/visualization/visualization_utils/utils_visual.py
@@ -1,5 +1,11 @@
 import settings
 import utils
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cpf
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import matplotlib.ticker as mticker
+import numpy as np
 
 
 def SizeTransport_load_data(scenario, prefix, data_direc, size,  rho, advection_data='CMEMS_MEDITERRANEAN',
@@ -22,3 +28,47 @@ def SizeTransport_load_data(scenario, prefix, data_direc, size,  rho, advection_
                                      init_density=rho, start_year=start_year, input=input, run=settings.RUN,
                                      restart=settings.RESTART)
     return data_direc + utils._analysis_save_file_name(input_file=file_name, prefix=prefix)
+
+
+def cartopy_standard_map(fig, gridspec, domain, resolution='50m', add_gridlines=True, add_gridlabels=True,
+                         lat_grid_step=20, lon_grid_step=30, label_size=14):
+    """
+    A nice basic function that can be used to create standardized maps
+    :param axis:
+    :return:
+    """
+    axis = fig.add_subplot(gridspec, projection=ccrs.PlateCarree())
+    # Setting the domain of the map
+    lon_min, lon_max, lat_min, lat_max = domain
+    axis.set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
+
+    # Adding coastlines, borders, land and ocean shapefiles
+    axis.coastlines(resolution=resolution)
+    axis.add_feature(cpf.BORDERS.with_scale(resolution), edgecolor='black', zorder=100)
+    axis.add_feature(cpf.LAND.with_scale(resolution), facecolor='grey')
+    axis.add_feature(cpf.OCEAN.with_scale(resolution), facecolor='white')
+
+    # Adding gridlines and axis labels
+    if add_gridlines:
+        grid = axis.gridlines(crs=ccrs.PlateCarree(),  # specify the projection being used
+                              draw_labels=add_gridlabels,  # Add labels
+                              linestyle='-',  # style
+                              )
+        # Here we can choose along which axes we want to have the labels included
+        grid.xlabels_top = False
+        grid.ylabels_left = False
+        # Formatting of the labels, since that they include N/S or E/W
+        grid.xformatter = LONGITUDE_FORMATTER
+        grid.yformatter = LATITUDE_FORMATTER
+
+        # Determine where we want labels
+        grid.xlocator = mticker.FixedLocator(np.arange(lon_min, lon_max + lon_grid_step, lon_grid_step))
+        grid.ylocator = mticker.FixedLocator(np.arange(lat_min, lat_max + lat_grid_step, lat_grid_step))
+        # Here we can change the appearances of the labels
+        grid.xlabel_style = {'size': label_size, 'color': 'black', 'weight': 'normal'}
+        grid.ylabel_style = {'size': label_size, 'color': 'black', 'weight': 'normal'}
+
+    # Setting the axis
+    axis.set_aspect('auto', adjustable=None)
+    return axis
+
