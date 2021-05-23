85e12a7f9f7ef18462584c880e8b685d6af0c2c1
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-23 17:28:20 +0200

    bug fix

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -21,7 +21,7 @@ def run(scenario, figure_direc: str):
 
     # Creating figures showing the relative distribution, averaged over the entire simulation and time-snapshots at the
     # end of each simulation year
-    time_select = 2
+    time_select = 'average'
     SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
                                                                                 scenario=scenario, size_list=size_list,
                                                                                 rho_list=rho_list,
