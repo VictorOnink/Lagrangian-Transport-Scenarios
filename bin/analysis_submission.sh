57d819ca2f55ab6d097edc21539a275624f7f1f9
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-25 10:02:26 +0200

    bug fix

--- bin/analysis_submission.sh
+++ bin/analysis_submission.sh
@@ -16,7 +16,7 @@ SHOREDEPEN=0
 #for scenario 4, the minimum wind speed for resusplension. Divide by 10 for actual value
 WMIN=3
 #for scenario 6, the initial size of the particle in 1e-5 m
-PARTICLE_SIZE=1
+PARTICLE_SIZE=500
 #the starting year of the simulation, and how many years the simulation will take
 STARTYEAR=2010
 #Which input distribution do we want to use? 0=Jambeck, 1=lebreton, 2=point release, 3=uniform release
@@ -51,11 +51,11 @@ export SERVER
 
 #A number of switches to indicate which analysis steps we want to run.
 #0 = off, 1 = on
-CONCENTRATION=1
-VERTICAL_CONCENTRATION=1
-TIMESERIES=0
+CONCENTRATION=0
+VERTICAL_CONCENTRATION=0
+TIMESERIES=1
 MAX_DISTANCE=0
-TIMESLICING=1
+TIMESLICING=0
 
 export CONCENTRATION
 export VERTICAL_CONCENTRATION
