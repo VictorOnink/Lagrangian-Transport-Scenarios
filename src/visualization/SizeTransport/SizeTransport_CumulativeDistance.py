8342e605cdd3b667b8e146f04ac83e7fe6a549c2
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-27 17:31:46 +0200

    bugfix

--- src/visualization/SizeTransport/SizeTransport_CumulativeDistance.py
+++ src/visualization/SizeTransport/SizeTransport_CumulativeDistance.py
@@ -62,15 +62,15 @@ def SizeTransport_CumulativeDistance(scenario, figure_direc, size_list, rho_list
     # First, we will plot the max depth
     ax_list[0].set_xlabel('Max depth (m)', fontsize=fontsize)
     ax_list[1].set_xscale('log')
-    # ax_list[1].set_xlim([1e0, 1e3])
+    ax_list[1].set_xlim([1e0, 1e3])
     # Next it will be the cumulative vertical distance
     ax_list[1].set_xlabel('Cumulative vertical distance (m)', fontsize=fontsize)
     ax_list[1].set_xscale('log')
-    # ax_list[1].set_xlim([1e0, 1e4])
+    ax_list[1].set_xlim([1e0, 1e4])
     # Finally, the cumulative horizontal distance
     ax_list[2].set_xlabel('Cumulative horizontal distance (km)', fontsize=fontsize)
     ax_list[2].set_xscale('log')
-    # ax_list[2].set_xlim([1e0, 1e4])
+    ax_list[2].set_xlim([1e0, 1e4])
 
     # Plotting the data
     for index_size, size in enumerate(size_list):
