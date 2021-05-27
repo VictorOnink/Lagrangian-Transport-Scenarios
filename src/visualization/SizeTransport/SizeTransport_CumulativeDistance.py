c6a68c973d4ee4408409769a4f3afa661ebfbb73
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-27 16:09:30 +0200

    bugfix

--- src/visualization/SizeTransport/SizeTransport_CumulativeDistance.py
+++ src/visualization/SizeTransport/SizeTransport_CumulativeDistance.py
@@ -41,8 +41,8 @@ def SizeTransport_CumulativeDistance(scenario, figure_direc, size_list, rho_list
             timeseries_dict[size][variable] = np.zeros(shape=variable_domain[index_var].shape)
             for step in range(len(timeseries_dict[size][variable])):
                 timeseries_dict[size][variable][step] += np.nansum(var_data < variable_domain[index_var][step])
-            timeseries_dict[size][variable] /= np.nansum(data_dict[variable]['total']['count'])
-            timeseries_dict[size][variable] *= 100.
+                timeseries_dict[size][variable][step] /= np.nansum(data_dict[variable]['total']['count'])
+                timeseries_dict[size][variable][step] *= 100.
 
     # Creating the figure structure
     fig = plt.figure(figsize=figsize)
