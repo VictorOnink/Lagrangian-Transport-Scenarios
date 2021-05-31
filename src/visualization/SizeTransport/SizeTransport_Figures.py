4bb591e8f5344a5e74e4e282e8f36a78de71f787
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-05-31 13:26:41 +0200

    bugfix

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -16,14 +16,14 @@ def run(scenario, figure_direc: str):
     :param scenario:
     :return:
     """
-    size_list = np.array([5000, 1000, 500, 100, 50, 10]) * settings.SIZE_FACTOR
+    size_list = np.array([5000, 500, 50, 10, 5, 1]) * settings.SIZE_FACTOR
     rho_list = np.ones(size_list.shape, dtype=int) * 920
 
     # Creating a figure of the basin bathymetry
     # General.General_bathymetry(scenario=scenario, figure_direc=figure_direc)
 
     # Figure of the mean wind speed
-    General.General_average_wind_speed(scenario=scenario, figure_direc=figure_direc)
+    # General.General_average_wind_speed(scenario=scenario, figure_direc=figure_direc)
 
     # Creating an animation showing how the six different size classes I have simulations for at the moment look like
     # SizeTransport_Animation.SizeTransport_Animation(figure_direc=figure_direc, scenario=scenario, size_list=size_list,
@@ -31,17 +31,17 @@ def run(scenario, figure_direc: str):
 
     # Creating figures showing the relative distribution, averaged over the entire simulation and time-snapshots at the
     # end of each simulation year
-    # time_select = 2
-    # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
-    #                                                                             scenario=scenario, size_list=size_list,
-    #                                                                             rho_list=rho_list,
-    #                                                                             time_selection=time_select,
-    #                                                                             beach_state='afloat')
-    # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
-    #                                                                             scenario=scenario, size_list=size_list,
-    #                                                                             rho_list=rho_list,
-    #                                                                             time_selection=time_select,
-    #                                                                             beach_state='seabed')
+    time_select = 0
+    SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
+                                                                                scenario=scenario, size_list=size_list,
+                                                                                rho_list=rho_list,
+                                                                                time_selection=time_select,
+                                                                                beach_state='afloat')
+    SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
+                                                                                scenario=scenario, size_list=size_list,
+                                                                                rho_list=rho_list,
+                                                                                time_selection=time_select,
+                                                                                beach_state='seabed')
     # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
     #                                                                             scenario=scenario, size_list=size_list,
     #                                                                             rho_list=rho_list,
@@ -49,23 +49,28 @@ def run(scenario, figure_direc: str):
     #                                                                             difference=True,
     #                                                                             beach_state='afloat')
 
+
+    size_list = np.array([5000, 1000, 500, 100, 50, 10, 5, 1]) * settings.SIZE_FACTOR
+    rho_list = np.ones(size_list.shape, dtype=int) * 920
+
     # Creating figures of the timeseries of the number of particles that are beached/afloat/seabed/removed
-    # SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
-    #                                                               size_list=size_list, rho_list=rho_list)
+    SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
+                                                                  size_list=size_list, rho_list=rho_list)
+
 
     # A histogram indicating at which depths particles end up beaching
-    # SizeTransport_SeaFloorDepthDistribution.SizeTransport_SeaFloorDepthDistribution(figure_direc=figure_direc,
-    #                                                                                 scenario=scenario,
-    #                                                                                 size_list=size_list,
-    #                                                                                 rho_list=rho_list, histogram=True)
-    # SizeTransport_SeaFloorDepthDistribution.SizeTransport_SeaFloorDepthDistribution(figure_direc=figure_direc,
-    #                                                                                 scenario=scenario,
-    #                                                                                 size_list=size_list,
-    #                                                                                 rho_list=rho_list, cumulative=True)
+    SizeTransport_SeaFloorDepthDistribution.SizeTransport_SeaFloorDepthDistribution(figure_direc=figure_direc,
+                                                                                    scenario=scenario,
+                                                                                    size_list=size_list,
+                                                                                    rho_list=rho_list, histogram=True)
+    SizeTransport_SeaFloorDepthDistribution.SizeTransport_SeaFloorDepthDistribution(figure_direc=figure_direc,
+                                                                                    scenario=scenario,
+                                                                                    size_list=size_list,
+                                                                                    rho_list=rho_list, cumulative=True)
 
     # Cumulative plots for the total distance travelled vertically and horizontally, and the max depth reached
-    # SizeTransport_CumulativeDistance.SizeTransport_CumulativeDistance(figure_direc=figure_direc,
-    #                                                                   scenario=scenario,
-    #                                                                   size_list=size_list,
-    #                                                                   rho_list=rho_list, )
+    SizeTransport_CumulativeDistance.SizeTransport_CumulativeDistance(figure_direc=figure_direc,
+                                                                      scenario=scenario,
+                                                                      size_list=size_list,
+                                                                      rho_list=rho_list, )
     pass
