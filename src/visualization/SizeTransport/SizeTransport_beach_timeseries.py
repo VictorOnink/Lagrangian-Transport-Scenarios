27d29aa257d1f40380389327fc06965fae6c5dc5
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-06-09 16:07:16 +0200

    analysis submission

--- src/visualization/SizeTransport/SizeTransport_beach_timeseries.py
+++ src/visualization/SizeTransport/SizeTransport_beach_timeseries.py
@@ -64,7 +64,7 @@ def SizeTransport_beach_timeseries(scenario, figure_direc, size_list, rho_list, 
         ax.xaxis.set_minor_locator(months)
         ax.xaxis.set_major_formatter(yearsFmt)
         ax.set_ylabel(r'Fraction of Total (%)', fontsize=fontsize)
-        ax.set_xlim(datetime(2010, 1, 1), datetime(2013, 1, 1))
+        ax.set_xlim(datetime(2010, 1, 1), datetime(2010, 1, 1))
         ax.set_ylim([0, 100])
         ax.tick_params(which='major', length=7)
         ax.tick_params(which='minor', length=3)
