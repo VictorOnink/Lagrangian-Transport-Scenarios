592226bed67cd91f4c709db9d17f7f2d60d72b72
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-06-21 17:02:33 +0200

    stupid bug fix

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -40,13 +40,13 @@ def run(scenario, figure_direc: str):
     #                                                                             rho_list=rho_list,
     #                                                                             time_selection=time_select,
     #                                                                             beach_state='afloat')
-    SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
-                                                                                scenario=scenario,
-                                                                                size_list=size_list,
-                                                                                rho_list=rho_list,
-                                                                                time_selection=time_select,
-                                                                                tau_list=tau_list,
-                                                                                beach_state='seabed')
+    # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
+    #                                                                             scenario=scenario,
+    #                                                                             size_list=size_list,
+    #                                                                             rho_list=rho_list,
+    #                                                                             time_selection=time_select,
+    #                                                                             tau_list=tau_list,
+    #                                                                             beach_state='seabed')
     # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
     #                                                                             scenario=scenario,
     #                                                                             size_list=size_list,
@@ -67,9 +67,9 @@ def run(scenario, figure_direc: str):
 
     #
     # # Creating figures of the timeseries of the number of particles that are beached/afloat/seabed/removed
-    # SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
-    #                                                               size_list=size_list, rho_list=rho_list,
-    #                                                               tau_list=tau_list)
+    SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
+                                                                  size_list=size_list, rho_list=rho_list,
+                                                                  tau_list=tau_list)
 
     # A histogram indicating at which depths particles end up beaching
     # SizeTransport_SeaFloorDepthDistribution.SizeTransport_SeaFloorDepthDistribution(figure_direc=figure_direc,
