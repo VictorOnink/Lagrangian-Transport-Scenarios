f00c6f82b1c41c589b7c97264a9d95158e54fc0a
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-06-21 16:22:57 +0200

    stupid bug fix

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -33,7 +33,7 @@ def run(scenario, figure_direc: str):
 
     # Creating figures showing the relative distribution, averaged over the entire simulation and time-snapshots at the
     # end of each simulation year
-    time_select = 1
+    time_select = 0
     # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
     #                                                                             scenario=scenario,
     #                                                                             size_list=size_list,
@@ -67,9 +67,9 @@ def run(scenario, figure_direc: str):
 
     #
     # # Creating figures of the timeseries of the number of particles that are beached/afloat/seabed/removed
-    SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
-                                                                  size_list=size_list, rho_list=rho_list,
-                                                                  tau_list=tau_list)
+    # SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
+    #                                                               size_list=size_list, rho_list=rho_list,
+    #                                                               tau_list=tau_list)
 
     # A histogram indicating at which depths particles end up beaching
     # SizeTransport_SeaFloorDepthDistribution.SizeTransport_SeaFloorDepthDistribution(figure_direc=figure_direc,
