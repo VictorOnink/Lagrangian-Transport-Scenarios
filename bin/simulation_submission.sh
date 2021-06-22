0ce0dab8ce08b3c0826ecdaaf671788a6e24543c
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-06-22 10:10:22 +0200

    stupid bug fix

--- bin/simulation_submission.sh
+++ bin/simulation_submission.sh
@@ -16,9 +16,9 @@ SHOREDEPEN=0
 #for scenario 4, the minimum wind speed for resuspension. Divide by 10 for actual value
 WMIN=3
 #for scenario 6, the initial size of the particle in 1e-6 m
-PARTICLE_SIZE=1
+PARTICLE_SIZE=5000
 #for scenario 6, the critical bottom shear stress for particle resuspension (x1e-3)
-SEABED_CRIT=140
+SEABED_CRIT=25
 #the starting year of the simulation, and how many years the simulation will take
 STARTYEAR=2010
 #Which input distribution do we want to use? 0=Jambeck, 1=lebreton, 2=point release, 3=uniform release
