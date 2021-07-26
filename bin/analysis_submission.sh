fa333a7d8b24bd81716cb6975608f11a73d04526
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-07-26 09:40:02 +0200

    job submission

--- bin/analysis_submission.sh
+++ bin/analysis_submission.sh
@@ -16,7 +16,7 @@ SHOREDEPEN=0
 #for scenario 4, the minimum wind speed for resusplension. Divide by 10 for actual value
 WMIN=3
 #for scenario 6, the initial size of the particle in 1e-5 m
-PARTICLE_SIZE_list=(5 1)
+PARTICLE_SIZE_list=(5000 1000 500 100 50 40 30 20 10 5 1)
 #for scenario 6, the critical bottom shear stress for particle resuspension (x1e-3)
 SEABED_CRIT=25
 #the starting year of the simulation, and how many years the simulation will take
@@ -27,7 +27,7 @@ INPUT=1
 # 0 = Global HYCOM, 1 = Caribbean HYCOM, 2 = Mediterranean CMEMS
 ADVECTION_DATA=2
 #Number of years the simulation runs
-SIMLEN=1
+SIMLEN=2
 #Inclusion of Stokes drift. 0 = include stokes, 1 = do not include stokes
 STOKES=0
 #Ensemble member
