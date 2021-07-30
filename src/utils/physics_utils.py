efbfb5dc30548ce2f65f1c6a475742ab023fab62
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-07-30 12:08:48 +0200

    update

--- src/utils/physics_utils.py
+++ src/utils/physics_utils.py
@@ -123,13 +123,11 @@ def floating_AdvectionRK4DiffusionEM_stokes_depth(particle, fieldset, time):
     if particle.beach == 0:
         t, d, la, lo, dt = time, particle.depth, particle.lat, particle.lon, particle.dt
         # Stokes depth dependence term
-        w_p = 2 * math.pi / fieldset.WP[t, d, la, lo]  # Peak period
+        w_p = math.fabs(2 * math.pi / fieldset.WP[t, d, la, lo])  # Peak period
         k_p = w_p ** 2 / 9.81  # peak wave number
         z_correc = max(d - fieldset.SURF_Z,
                        0)  # correction since the surface in the CMEMS Mediterranean data is not at 0
         st_z = math.exp(-2 * k_p * z_correc) - math.sqrt(2 * math.pi * k_p * z_correc) * math.erfc(2 * k_p * z_correc)
-        print(st_z)
-        st_z=1
 
         # RK4 terms
         (u1, v1) = fieldset.UV[t, d, la, lo]
