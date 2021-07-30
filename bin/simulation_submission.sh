cb96425ce792cca17d7a444e14659c9a9ee4e4ac
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-07-30 12:38:09 +0200

    update

--- bin/simulation_submission.sh
+++ bin/simulation_submission.sh
@@ -2,7 +2,7 @@
 # General run parameters                                                            #
 #####################################################################################
 SUBMISSION='simulation'
-DEBUG=1 # 0 = Not a debug run, 1 = a debug run
+DEBUG=0 # 0 = Not a debug run, 1 = a debug run
 #0=first order, 1=coastal, 2=stochastic beaching/resuspension, 3=coast type dependent, 4 = Turrell (2020)
 #5 = Kaandorp based fragmentation, 6 = Size dependent transport
 SCENARIO=6
