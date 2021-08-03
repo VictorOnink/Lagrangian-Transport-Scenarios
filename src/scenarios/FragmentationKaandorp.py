0230ca6380e77e8477ab27e0fe28c6c081b6f721
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-03 10:03:41 +0200

    update

--- src/scenarios/FragmentationKaandorp.py
+++ src/scenarios/FragmentationKaandorp.py
@@ -165,7 +165,8 @@ class FragmentationKaandorp(base_scenario.BaseScenario):
                          pset.Kernel(utils.floating_AdvectionRK4DiffusionEM_stokes_depth) + \
                          pset.Kernel(utils.anti_beach_nudging) + \
                          pset.Kernel(utils.get_rising_velocity) + \
-                         pset.Kernel(utils.KPP_TIDAL_mixing)
+                         pset.Kernel(utils.KPP_wind_mixing) + \
+                         pset.Kernel(utils.internal_tide_mixing)
         return total_behavior
 
     def fragmentation_kernel(particle, fieldset, time):
