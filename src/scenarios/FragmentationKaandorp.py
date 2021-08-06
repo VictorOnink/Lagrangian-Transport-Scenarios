bd08261e4a93f258091895dd309ab2286f98fb61
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-06 11:13:17 +0200

    update

--- src/scenarios/FragmentationKaandorp.py
+++ src/scenarios/FragmentationKaandorp.py
@@ -230,9 +230,6 @@ class FragmentationKaandorp(base_scenario.BaseScenario):
                              repeat_dt=self.repeat_dt)
         pfile = pset.ParticleFile(name=self.file_names(new=True),
                                   outputdt=settings.OUTPUT_TIME_STEP)
-        vardict=self.get_var_dict()
-        utils.print_statement(vardict['size'][::1000])
-        utils.print_statement(utils.initial_estimate_particle_rise_velocity(L=vardict['size'][::1000]))
         # Setting the random seed and defining the particle behavior
         utils.print_statement("Setting the random seed")
         utils.set_random_seed(seed=settings.SEED)
