8c25782245d9185a7fa97c0111d1efca8efeedb1
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-09 15:05:01 +0200

    update

--- src/scenarios/FragmentationKaandorp.py
+++ src/scenarios/FragmentationKaandorp.py
@@ -224,6 +224,7 @@ class FragmentationKaandorp(base_scenario.BaseScenario):
 
     def run(self):
         utils.print_statement(self.field_set.p_frag)
+        utils.print_statement(self.field_set.p_beach)
         # Creating the particle set and output file
         # pset = self.get_pset(fieldset=self.field_set, particle_type=self.particle,
         #                      var_dict=self.get_var_dict(), start_time=utils.get_start_end_time(time='start'),
