35f26a9cd1a3d42e9f11f35be83efb5bc3e9441f
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-11 11:33:00 +0200

    update

--- src/Analysis/parcels_to_sizespectrum.py
+++ src/Analysis/parcels_to_sizespectrum.py
@@ -22,4 +22,4 @@ def parcels_to_sizespectrum(file_dict: dict):
         parcels_file = file_dict[0][restart]
         parcels_dataset = Dataset(parcels_file)
         time_list = np.append(time_list, np.unique(parcels_dataset.variables['time'][:, :-1]))
-    utils.print_statement('we have {} time points'.format(time_list.size))
+    utils.print_statement('we have {} time points'.format(time_list.size), to_print=True)
