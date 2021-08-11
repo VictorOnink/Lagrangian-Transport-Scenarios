f687c565bc02540c03072495c2c91d25bdf6eb28
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-11 15:59:51 +0200

    update

--- src/visualization/FragmentationKaandorp/FragmentationKaandorp_SizeSpectrumTime.py
+++ src/visualization/FragmentationKaandorp/FragmentationKaandorp_SizeSpectrumTime.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
 
-def FragmentationKaandorp_SizeSpectrumTime(figure_direc, scenario, shore_time, lambda_frag_list):
+def FragmentationKaandorp_SizeSpectrumTime(figure_direc, scenario, shore_time, lambda_frag_list, figsize=(12, 10),
+                                           fontsize=12):
     # Setting the folder within which we have the output, and where we have the saved data
     output_direc = figure_direc + 'size_distribution/'
     utils.check_direc_exist(output_direc)
@@ -24,3 +25,29 @@ def FragmentationKaandorp_SizeSpectrumTime(figure_direc, scenario, shore_time, l
     size_bins = data_dict['size_bins']
     for keys in size_dict.keys():
         utils.print_statement(keys, to_print=True)
+
+    # Creating the figure
+    fig = plt.figure(figsize=figsize)
+    gs = fig.add_gridspec(nrows=2, ncols=4, width_ratios=[1, 1, 1, 0.2])
+    gs.update(wspace=0.2, hspace=0.2)
+
+    ax_list = []
+    for row in range(gs.nrows):
+        for column in range(gs.ncols - 1):
+            ax = fig.add_subplot(gs[row, 0])
+            if column == 0:
+                ax.set_ylabel(r'Particles', fontsize=fontsize)
+            if row == (gs.nrows - 1):
+                ax.set_xlabel(r'Size (m)', fontsize=fontsize)
+            ax.set_xlim([size_bins.min(), size_bins.max()])
+            ax.set_ylim([0, 1e4])
+            ax.tick_params(which='major', length=7)
+            ax.tick_params(which='minor', length=3)
+            ax.set_yscale('log')
+            ax.set_xscale('log')
+            if row != (gs.nrows - 1):
+                ax.set_xticklabels([])
+            ax_list.append(ax)
+
+    file_name = output_direc + 'SizeSpectrumTime-ST={}.png'.format(shore_time)
+    plt.savefig(file_name, bbox_inches='tight')
