8faa145e8061520e9c3e0685ea29f7c770bcd732
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-11 14:24:39 +0200

    update

--- src/Analysis/parcels_to_sizespectrum.py
+++ src/Analysis/parcels_to_sizespectrum.py
@@ -16,6 +16,10 @@ def parcels_to_sizespectrum(file_dict: dict):
     # Setting the size bins
     size_bins = np.logspace(start=-6, stop=-2, num=50)
 
+    output_direc = utils.get_output_directory(server=settings.SERVER) + 'size_distribution/{}/'.format(
+        settings.SCENARIO_NAME)
+    utils.check_direc_exist(output_direc)
+
     # Loading the time axis
     time_list = np.array([], dtype=np.int)
     for restart in range(settings.SIM_LENGTH):
@@ -25,4 +29,28 @@ def parcels_to_sizespectrum(file_dict: dict):
         for time_case in np.unique(time):
             if np.nansum(time_case == time) > 10 and time_case != np.nan:
                 time_list = np.append(time_list, time_case)
-    utils.print_statement(len(time_list), to_print=True)
+
+    # Creating the output dict
+    output_dict = {'size_bins': size_bins}
+
+    # loop through the runs
+    for run in range(settings.RUN_RANGE):
+        # Loop through the restart files
+        for restart in range(settings.SIM_LENGTH):
+            # Load the lon, lat, time, beach and weight data
+            parcels_file = file_dict[run][restart]
+            parcels_dataset = Dataset(parcels_file)
+            time = parcels_dataset.variables['time'][:, :-1]
+            size = parcels_dataset.variables['size'][:, :-1]
+
+            # Calculating the spectrum every 30 days
+            for index_time, time_select in enumerate(range(0, len(time_list), 60)):
+                size_selection = size[time_select == time]
+                size_counts, _ = np.histogram(size_selection, bins=size_bins)
+                output_dict[index_time] = size_counts
+
+    # Saving the output
+    prefix = 'size_distribution'
+    output_name = output_direc + utils.analysis_save_file_name(input_file=file_dict[0][0], prefix=prefix)
+    utils.save_obj(output_name, output_dict)
+    utils.print_statement("The timeseries has been saved")
