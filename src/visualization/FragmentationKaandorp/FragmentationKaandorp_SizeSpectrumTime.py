ed3545810931762b380f9571b47151e74c47ea33
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-12 10:01:05 +0200

    update

--- src/visualization/FragmentationKaandorp/FragmentationKaandorp_SizeSpectrumTime.py
+++ src/visualization/FragmentationKaandorp/FragmentationKaandorp_SizeSpectrumTime.py
@@ -67,7 +67,7 @@ def FragmentationKaandorp_SizeSpectrumTime(figure_direc, scenario, shore_time, l
     size_colors = [plt.plot([], [], c=vUtils.discrete_color_from_cmap(month, subdivisions=12 // month_step),
                             label='t = {} months'.format(month * 3), linestyle='-')[0] for month in range(12 // month_step)]
 
-    cbar = ax_legend.legend(handles=size_colors, cax=ax_legend, fontsize=fontsize)
+    cbar = ax_legend.legend(handles=size_colors, fontsize=fontsize)
     ax_legend.axis('off')
 
     file_name = output_direc + 'SizeSpectrumTime-ST={}.png'.format(shore_time)
