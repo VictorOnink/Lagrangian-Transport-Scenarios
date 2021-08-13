b725c9ffbf848bbb101a8eac280f624d9238f5ca
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-13 17:37:06 +0200

    update

--- bin/simulation_submission.sh
+++ bin/simulation_submission.sh
@@ -34,7 +34,7 @@ INPUT=1
 # 0 = Global HYCOM, 1 = Caribbean HYCOM, 2 = Mediterranean CMEMS
 ADVECTION_DATA=2
 #Start year of the simulation. 0 = new simulation, otherwise it picks up from a previous simulation
-START=0
+START=2
 #Number of years the simulation runs
 SIMLEN=3
 #Inclusion of Stokes drift. 0 = include stokes, 1 = do not include stokes
