85c15b94167e033472c33f86dca23fc82345aff8
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-17 11:38:34 +0200

    update

--- bin/simulation_submission.sh
+++ bin/simulation_submission.sh
@@ -5,7 +5,7 @@ SUBMISSION='simulation'
 DEBUG=0 # 0 = Not a debug run, 1 = a debug run
 #0=first order, 1=coastal, 2=stochastic beaching/resuspension, 3=coast type dependent, 4 = Turrell (2020)
 #5 = Size dependent transport, 6 = Kaandorp based fragmentation, 7 = alternate Kaandorp fragmentation
-SCENARIO=6
+SCENARIO=7
 #for scenario 1, the time a particle must be near the coast to beach (in days)
 VICINITY=2
 #for scenario 2, the beaching and resuspension timescales (in days)
