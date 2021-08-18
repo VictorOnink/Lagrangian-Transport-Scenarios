3997cfbc589b4e8f50e2271b6da234d9992ed66c
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-18 15:32:58 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Animation.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Animation.py
@@ -54,7 +54,7 @@ def FragmentationKaandorpPartial_Animation(scenario, figure_direc, shore_time, l
     cbar.ax.tick_params(which='minor', labelsize=tick_label_size, length=7, width=2)
 
     # Setting the time range for which we want to create the simulation
-    current_time = datetime(2010, 1, 1, 0)
+    current_time = datetime(2010, 5, 1, 0)
     end_time = datetime(2010, 12, 31, 12)
     time_step = timedelta(hours=12)
     time_list = []
@@ -67,55 +67,43 @@ def FragmentationKaandorpPartial_Animation(scenario, figure_direc, shore_time, l
     props = dict(boxstyle='round', facecolor='white', alpha=1)
     text = ax_list[0].text(0.02, 0.02, 'initial', horizontalalignment='left', verticalalignment='bottom',
                    transform=ax_list[0].transAxes, bbox=props, fontsize=ax_label_size, zorder=200)
-    plt.savefig(animation_save_name(output_direc=output_direc, shore_time=shore_time, lambda_frag=lambda_frag, file_type='.jpg'))
-    # # Now, the actual animation part
-    # # Setting the initial values of the x and y, which will later be filled by lon and lat
-    # plot_list = []
-    # for ax in ax_list:
-    #     plot_list.append(ax.scatter(0, 0, c=0, s=4, alpha=1, zorder=1000, cmap='inferno_r', norm=norm))
-    #
-    # # Initializing the plots on each axis
-    # def init():
-    #     for plot in plot_list:
-    #         plot.set_offsets(np.c_[[], []])
-    #     text.set_text('initial 2')
-    #     return plot_list
-    #
-    # def animate(frame_index):
-    #     utils.print_statement("we are at index {} of {}".format(frame_index, frame_number), to_print=True)
-    #     date = time_list[frame_index].strftime("%Y-%m-%d-%H-%M-%S")
-    #     for index, size in enumerate(size_list):
-    #         # Loading the dictionary with the data
-    #         prefix = 'timeslices_{}'.format(date)
-    #         data_dict = vUtils.SizeTransport_load_data(scenario=scenario, prefix=prefix, data_direc=data_direc,
-    #                                                    size=size, rho=rho_list[index], tau=tau_list[index])
-    #         lon, lat, depth = data_dict['lon'], data_dict['lat'], data_dict['z'].astype(int)
-    #         # Updating the plot on each axis with the data
-    #         plot_list[index].set_offsets(np.c_[lon, lat])
-    #         plot_list[index].set_array(depth)
-    #     text.set_text(time_list[frame_index].strftime("%Y-%m-%d"))
-    #     return plot_list
-    #
-    # # Calling the animator
-    # animator = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
-    #                                    frames=frame_number, interval=100, blit=True)
-    #
-    # # Saving the animation
-    # animator.save(filename=animation_save_name(output_direc=output_direc, shore_time=shore_time,
-    #                                            lambda_frag=lambda_frag),
-    #               fps=10, extra_args=['-vcodec', 'libx264'])
 
+    # Now, the actual animation part
+    # Setting the initial values of the x and y, which will later be filled by lon and lat
+    plot_list = []
+    for ax in ax_list:
+        plot_list.append(ax.scatter(0, 0, c=0, s=4, alpha=1, zorder=1000, cmap='inferno_r', norm=norm))
 
-def subfigure_title(index, size, rho):
-    """
-    setting the title of the subfigure
-    :param index:
-    :param size:
-    :param rho:
-    :return:
-    """
-    alphabet = string.ascii_lowercase
-    return '({}) r = {:.3f} mm, '.format(alphabet[index], size * 1e3) + r'$\rho$ = ' + '{} kg m'.format(rho) + r'$^{-3}$'
+    # Initializing the plots on each axis
+    def init():
+        for plot in plot_list:
+            plot.set_offsets(np.c_[[], []])
+        text.set_text('initial 2')
+        return plot_list
+
+    def animate(frame_index):
+        utils.print_statement("we are at index {} of {}".format(frame_index, frame_number), to_print=True)
+        date = time_list[frame_index].strftime("%Y-%m-%d-%H-%M-%S")
+        for ax_index, size in enumerate(ax_list):
+            # Loading the dictionary with the data
+            prefix = 'timeslices_{}'.format(date)
+            data_dict = vUtils.FragmentationKaandorpPartial_load_data(scenario=scenario, prefix=prefix, data_direc=data_direc,
+                                                                      lambda_frag=lambda_frag, rho=920, shore_time=shore_time)
+            lon, lat, depth = data_dict['lon'], data_dict['lat'], data_dict['z'].astype(int)
+            # Updating the plot on each axis with the data
+            plot_list[ax_index].set_offsets(np.c_[lon, lat])
+            plot_list[ax_index].set_array(depth)
+        text.set_text(time_list[frame_index].strftime("%Y-%m-%d"))
+        return plot_list
+
+    # Calling the animator
+    animator = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
+                                       frames=2, interval=100, blit=True)
+
+    # Saving the animation
+    animator.save(filename=animation_save_name(output_direc=output_direc, shore_time=shore_time,
+                                               lambda_frag=lambda_frag),
+                  fps=10, extra_args=['-vcodec', 'libx264'])
 
 
 def animation_save_name(output_direc, shore_time, lambda_frag, flowdata='CMEMS_MEDITERRANEAN', file_type='.mov'):
