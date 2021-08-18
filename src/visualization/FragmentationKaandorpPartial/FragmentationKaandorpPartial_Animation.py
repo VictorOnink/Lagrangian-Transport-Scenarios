8e95f66b9efb8f90a4ef4c286d6474efa48d1752
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-18 15:45:39 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Animation.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Animation.py
@@ -6,9 +6,8 @@ import matplotlib.colors as colors
 from advection_scenarios import advection_files
 import numpy as np
 from datetime import datetime, timedelta
-import string
 import matplotlib.animation as animation
-import os
+import cmocean.cm as cmo
 
 
 def FragmentationKaandorpPartial_Animation(scenario, figure_direc, shore_time, lambda_frag,
@@ -46,7 +45,7 @@ def FragmentationKaandorpPartial_Animation(scenario, figure_direc, shore_time, l
     # Setting the colormap, that we will use for coloring the scatter plot according to the particle depth. Then, adding
     # a colorbar.
     norm = colors.Normalize(vmin=0.0, vmax=100.0)
-    cmap = plt.cm.ScalarMappable(cmap='plasma_r', norm=norm)
+    cmap = plt.cm.ScalarMappable(cmap=cmo.deep, norm=norm)
     cax = fig.add_subplot(gs[:, -1])
     cbar = plt.colorbar(cmap, cax=cax, orientation='vertical', extend='max')
     cbar.set_label(r"Depth (m)", fontsize=ax_label_size)
@@ -54,7 +53,7 @@ def FragmentationKaandorpPartial_Animation(scenario, figure_direc, shore_time, l
     cbar.ax.tick_params(which='minor', labelsize=tick_label_size, length=7, width=2)
 
     # Setting the time range for which we want to create the simulation
-    current_time = datetime(2010, 5, 1, 0)
+    current_time = datetime(2010, 11, 1, 0)
     end_time = datetime(2010, 12, 31, 12)
     time_step = timedelta(hours=12)
     time_list = []
@@ -72,7 +71,7 @@ def FragmentationKaandorpPartial_Animation(scenario, figure_direc, shore_time, l
     # Setting the initial values of the x and y, which will later be filled by lon and lat
     plot_list = []
     for ax in ax_list:
-        plot_list.append(ax.scatter(0, 0, c=0, s=4, alpha=1, zorder=1000, cmap='inferno_r', norm=norm))
+        plot_list.append(ax.scatter(0, 0, c=0, s=4, alpha=1, zorder=1000, cmap=cmo.deep, norm=norm))
 
     # Initializing the plots on each axis
     def init():
