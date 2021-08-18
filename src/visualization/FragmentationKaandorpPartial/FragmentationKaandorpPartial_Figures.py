b670f07cfeabddef348020d4a82c1e4a0d80978f
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-18 11:19:17 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
@@ -1,11 +1,12 @@
 import settings
 import numpy as np
-import visualization.FragmentationKaandorpPartial.FragmentationKaandorpPartial_SizeSpectrumTime as FragmentationKaandorpPartial_SizeSpectrumTime
+import visualization.FragmentationKaandorpPartial.FragmentationKaandorpPartial_SizeSpectrumTime as SizeSpectrumTime
 
 
 def run(scenario, figure_direc: str):
     shore_time_list = np.array([1, 20])
     lambda_frag_list = np.array([1, 10, 100, 200, 300, 388])
 
-    FragmentationKaandorpPartial_SizeSpectrumTime.run(figure_direc=figure_direc, scenario=scenario, shore_time=20,
-                                                      lambda_frag_list=lambda_frag_list, density=920)
+    SizeSpectrumTime.FragmentationKaandorpPartial_SizeSpectrumTime(figure_direc=figure_direc, scenario=scenario,
+                                                                   shore_time=20, lambda_frag_list=lambda_frag_list,
+                                                                   density=920)
