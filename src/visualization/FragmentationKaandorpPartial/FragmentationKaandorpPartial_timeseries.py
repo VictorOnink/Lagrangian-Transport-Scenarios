d05bbea51c40b321c18a8b16ea60455f5f1bc455
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-19 17:03:28 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_timeseries.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_timeseries.py
@@ -26,10 +26,10 @@ def FragmentationKaandorpPartial_timeseries(scenario, figure_direc, shore_time, 
                                                                   shore_time=shore_time, lambda_frag=lambda_frag, rho=rho)
         for beach_state in beach_state_list:
             timeseries_dict[size_class][beach_state] = data_dict[beach_state][size_class]
-        timeseries_dict[size_class]['time_raw'] = data_dict['time'][size_class]
+
     # creating a time axis
     time_list = []
-    for time in timeseries_dict[size_class]['time_raw']:
+    for time in data_dict['time'][size_class]:
         time_list.append(datetime(settings.START_YEAR, 1, 1, 12) + timedelta(seconds=time))
 
     # Creating the axis
