f6773135895c14c91323220c18e8b2d5b505c9c9
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-20 11:20:26 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
@@ -11,9 +11,9 @@ def run(scenario, figure_direc: str):
     lambda_frag_list = np.array([1, 10, 100, 200, 300, 388])
     rho = 920
 
-    SizeSpectrumTime.FragmentationKaandorpPartial_SizeSpectrumTime(figure_direc=figure_direc, scenario=scenario,
-                                                                   shore_time=20, lambda_frag_list=lambda_frag_list,
-                                                                   density=rho)
+    # SizeSpectrumTime.FragmentationKaandorpPartial_SizeSpectrumTime(figure_direc=figure_direc, scenario=scenario,
+    #                                                                shore_time=20, lambda_frag_list=lambda_frag_list,
+    #                                                                density=rho)
 
     # Animation.FragmentationKaandorpPartial_Animation(scenario=scenario, figure_direc=figure_direc, shore_time=20,
     #                                                  lambda_frag=388, rho=rho)
@@ -22,3 +22,7 @@ def run(scenario, figure_direc: str):
     #                                                    lambda_frag=300, rho=rho, simulation_length=1)
     # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc, shore_time=20,
     #                                                    lambda_frag=388, rho=rho, simulation_length=1)
+
+    SizeSpectrumBeach.FragmentationKaandorpPartial_SizeSpectrumBeach(figure_direc=figure_direc, scenario=scenario,
+                                                                     shore_time=20, lambda_frag_list=388,
+                                                                     density=rho)
--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_SizeSpectrumBeach.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_SizeSpectrumBeach.py
@@ -7,7 +7,7 @@ import string
 import numpy as np
 
 
-def FragmentationKaandorpPartial_SizeSpectrumBeach(figure_direc, scenario, shore_time, lambda_frag_list, density,
+def FragmentationKaandorpPartial_SizeSpectrumBeach(figure_direc, scenario, shore_time, lambda_frag, density,
                                                   fig_size=(18, 12), x_label='Size (m)', y_label='Number of Particles',
                                                   ax_ticklabel_size=12, ax_label_size=14, legend_size=14):
     # Setting the folder within which we have the output, and where we have the saved data
@@ -17,13 +17,35 @@ def FragmentationKaandorpPartial_SizeSpectrumBeach(figure_direc, scenario, shore
 
     # Loading in the data
     prefix = 'size_distribution'
-    size_dict = {}
-    for lambda_frag in lambda_frag_list:
-        data_dict = vUtils.FragmentationKaandorpPartial_load_data(scenario=scenario, prefix=prefix,
-                                                                  data_direc=data_direc, shore_time=shore_time,
-                                                                  lambda_frag=lambda_frag, rho=density)
-        size_dict[lambda_frag] = {}
-        for month in data_dict.keys():
-            if month != 'size_bins':
-                size_dict[lambda_frag][month] = data_dict[month]
-    size_bins = data_dict['size_bins'][:-1]
+    data_dict = vUtils.FragmentationKaandorpPartial_load_data(scenario=scenario, prefix=prefix,
+                                                              data_direc=data_direc, shore_time=shore_time,
+                                                              lambda_frag=lambda_frag, rho=density)
+    time_index = data_dict['final_index']
+    beach_state_list = ['total', 'beach', 'seabed', 'afloat', 'afloat_5m']
+
+    # Creating the figure
+    ax_range = 1e-2, 1e-5, 1e7, 1e-2
+    plot_num = 5
+    ax = vUtils.base_figure(fig_size=fig_size, ax_range=ax_range, x_label=x_label, y_label=y_label,
+                            ax_ticklabel_size=ax_ticklabel_size, ax_label_size=ax_label_size, shape=(5, 1),
+                            plot_num=plot_num, log_yscale=True, log_xscale=True, all_x_labels=True)
+
+    # Labelling the subfigures
+    for index_ax in range(plot_num):
+        ax[index_ax].set_title(subfigure_title(index_ax, beach_state_list[index_ax]), fontsize=ax_label_size)
+
+    file_name = output_direc + 'SizeSpectrumBeach-ST={}-rho={}-lamf={}.png'.format(shore_time, density, lambda_frag)
+    plt.savefig(file_name, bbox_inches='tight')
+
+
+def subfigure_title(index, beach_state):
+    """
+    setting the title of the subfigure
+    :param index:
+    :param size:
+    :param rho:
+    :return:
+    """
+    alphabet = string.ascii_lowercase
+    return '({}) {}'.format(alphabet[index], beach_state)
+
