641e3f29460fc9dee91c11b9a2010c13981ca4cc
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-23 09:11:14 +0200

    update

--- src/scenarios/FragmentationKaandorpPartial.py
+++ src/scenarios/FragmentationKaandorpPartial.py
@@ -65,10 +65,11 @@ class FragmentationKaandorpPartial(base_scenario.BaseScenario):
                                time=start_time, repeatdt=repeat_dt)
         else:
             rise_velocity = utils.initial_estimate_particle_rise_velocity(L=var_dict['size'])
+            rho_plastic = np.ones(rise_velocity.shape, dtype=np.float32) * settings.INIT_DENSITY
             pset = ParticleSet(fieldset=fieldset, pclass=particle_type,
                                lon=var_dict['lon'], lat=var_dict['lat'], beach=var_dict['beach'], parent=var_dict['parent'],
                                age=var_dict['age'], size=var_dict['size'], beach_time=var_dict['beach_time'],
-                               rho_plastic=var_dict['rho_plastic'], rise_velocity=rise_velocity,
+                               rho_plastic=rho_plastic, rise_velocity=rise_velocity,
                                prob_resus=utils.resuspension_probability(w_rise=rise_velocity),
                                size_class=var_dict['size_class'], particle_number=var_dict['particle_number'],
                                time=start_time, repeatdt=repeat_dt)
