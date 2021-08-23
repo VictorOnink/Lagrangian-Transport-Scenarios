c8f2c2a82f497f9dcdd4fe3f9d81012a6f3ccc79
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-23 09:54:51 +0200

    update

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -31,42 +31,42 @@ def run(scenario, figure_direc: str):
     # General.General_input_scenario(scenario=scenario, figure_direc=figure_direc)
 
     # Creating an animation showing how the six different size classes I have simulations for at the moment look like
-    # SizeTransport_Animation.SizeTransport_Animation(figure_direc=figure_direc, scenario=scenario, size_list=size_list,
-    #                                                 rho_list=rho_list, tau_list=tau_list)
+    SizeTransport_Animation.SizeTransport_Animation(figure_direc=figure_direc, scenario=scenario, size_list=size_list,
+                                                    rho_list=rho_list, tau_list=tau_list)
 
     # Creating figures showing the relative distribution, averaged over the entire simulation and time-snapshots at the
     # end of each simulation year
     time_select = 2
-    SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
-                                                                                scenario=scenario,
-                                                                                size_list=size_list,
-                                                                                rho_list=rho_list,
-                                                                                tau_list=tau_list,
-                                                                                time_selection=time_select,
-                                                                                beach_state='afloat')
-    SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
-                                                                                scenario=scenario,
-                                                                                size_list=size_list,
-                                                                                rho_list=rho_list,
-                                                                                time_selection=time_select,
-                                                                                tau_list=tau_list,
-                                                                                beach_state='seabed')
-    SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
-                                                                                scenario=scenario,
-                                                                                size_list=size_list,
-                                                                                rho_list=rho_list,
-                                                                                tau_list=tau_list,
-                                                                                time_selection=time_select,
-                                                                                beach_state='beach')
+    # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
+    #                                                                             scenario=scenario,
+    #                                                                             size_list=size_list,
+    #                                                                             rho_list=rho_list,
+    #                                                                             tau_list=tau_list,
+    #                                                                             time_selection=time_select,
+    #                                                                             beach_state='afloat')
+    # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
+    #                                                                             scenario=scenario,
+    #                                                                             size_list=size_list,
+    #                                                                             rho_list=rho_list,
+    #                                                                             time_selection=time_select,
+    #                                                                             tau_list=tau_list,
+    #                                                                             beach_state='seabed')
+    # SizeTransport_relative_concentrations.SizeTransport_relative_concentrations(figure_direc=figure_direc,
+    #                                                                             scenario=scenario,
+    #                                                                             size_list=size_list,
+    #                                                                             rho_list=rho_list,
+    #                                                                             tau_list=tau_list,
+    #                                                                             time_selection=time_select,
+    #                                                                             beach_state='beach')
 
-    size_list = np.array([5000, 1000, 500, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 1]) * settings.SIZE_FACTOR
-    rho_list = np.ones(size_list.shape, dtype=int) * 920
-    tau_list = np.array([0.14])
+    # size_list = np.array([5000, 1000, 500, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 1]) * settings.SIZE_FACTOR
+    # rho_list = np.ones(size_list.shape, dtype=int) * 920
+    # tau_list = np.array([0.14])
 
     # Creating figures of the timeseries of the number of particles that are beached/afloat/seabed/removed
-    SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
-                                                                  size_list=size_list, rho_list=rho_list,
-                                                                  tau_list=tau_list, simulation_years=3)
+    # SizeTransport_beach_timeseries.SizeTransport_beach_timeseries(figure_direc=figure_direc, scenario=scenario,
+    #                                                               size_list=size_list, rho_list=rho_list,
+    #                                                               tau_list=tau_list, simulation_years=3)
 
     # A histogram indicating at which depths particles end up beaching
     # SizeTransport_SeaFloorDepthDistribution.SizeTransport_SeaFloorDepthDistribution(figure_direc=figure_direc,
@@ -80,16 +80,16 @@ def run(scenario, figure_direc: str):
     #                                                                                 cumulative=True)
 
     # Cumulative plots for the total distance travelled vertically and horizontally, and the max depth reached
-    SizeTransport_CumulativeDistance.SizeTransport_CumulativeDistance(figure_direc=figure_direc,
-                                                                      scenario=scenario,
-                                                                      size_list=size_list,
-                                                                      rho_list=rho_list,
-                                                                      tau_list=tau_list)
+    # SizeTransport_CumulativeDistance.SizeTransport_CumulativeDistance(figure_direc=figure_direc,
+    #                                                                   scenario=scenario,
+    #                                                                   size_list=size_list,
+    #                                                                   rho_list=rho_list,
+    #                                                                   tau_list=tau_list)
 
     # Plotting the separation distance
-    for size_selection in size_list:
-        SizeTransport_SeparationDistance.SizeTransport_SeparationDistance(scenario=scenario, figure_direc=figure_direc,
-                                                                          size_selection=size_selection,
-                                                                          rho_selection=920, tau_selection=0.14,
-                                                                          size_list=size_list)
+    # for size_selection in size_list:
+    #     SizeTransport_SeparationDistance.SizeTransport_SeparationDistance(scenario=scenario, figure_direc=figure_direc,
+    #                                                                       size_selection=size_selection,
+    #                                                                       rho_selection=920, tau_selection=0.14,
+    #                                                                       size_list=size_list)
     pass
