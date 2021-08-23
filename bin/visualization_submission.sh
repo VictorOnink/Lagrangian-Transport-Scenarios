acd096a4daf4305ac00f0c051299f0d333051c9d
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-23 09:55:04 +0200

    update

--- bin/visualization_submission.sh
+++ bin/visualization_submission.sh
@@ -58,7 +58,7 @@ if [ "$SERVER" -eq "1" ]; then
   part5="#SBATCH --output="runOutput/$runname".o%j"
   part6="#SBATCH --mem-per-cpu=20G"
   if [ "$DEBUG" -eq "0" ]; then
-        part7="#SBATCH --time=20:00:00"
+        part7="#SBATCH --time=24:00:00"
         part8="#SBATCH --partition=epyc2"
         part9='#SBATCH --qos=job_epyc2'
   else
