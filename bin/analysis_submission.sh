47b617f9fcf608a216c4ed5008af8187884378ad
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-27 12:37:32 +0200

    update

--- bin/analysis_submission.sh
+++ bin/analysis_submission.sh
@@ -2,7 +2,7 @@
 # First we define the general parameters of the run                                 #
 #####################################################################################
 SUBMISSION='analysis'
-DEBUG=1 # 0 = Not a debug run, 1 = a debug run
+DEBUG=0 # 0 = Not a debug run, 1 = a debug run
 #0=first order, 1=coastal, 2=stochastic beaching/resuspension, 3=coast type dependent, 4 = Turrell (2020)
 #5 = Size dependent transport, 6 = Kaandorp based fragmentation, 7 = alternate Kaandorp fragmentation
 SCENARIO=7
