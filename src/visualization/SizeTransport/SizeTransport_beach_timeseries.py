4f1bd1de625a73f4f0f25ea2728d280f60a8e81d
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-08-31 13:58:10 +0200

    update

--- src/visualization/SizeTransport/SizeTransport_beach_timeseries.py
+++ src/visualization/SizeTransport/SizeTransport_beach_timeseries.py
@@ -28,6 +28,7 @@ def SizeTransport_beach_timeseries(scenario, figure_direc, size_list, rho_list, 
             timeseries_dict[size][tau] = {}
             for beach_state in beach_state_list:
                 timeseries_dict[size][tau][beach_state] = data_dict[beach_state]
+                print(beach_state)
                 print(data_dict[beach_state])
             timeseries_dict[size][tau]['time_raw'] = data_dict['time']
             timeseries_dict[size][tau]['total'] = data_dict['total'][0]
