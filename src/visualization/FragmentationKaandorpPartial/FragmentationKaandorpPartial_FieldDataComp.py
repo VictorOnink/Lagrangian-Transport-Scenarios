c9e0602c5e3596029706b46c9c231037f8dcb376
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-09-07 15:49:47 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
@@ -7,7 +7,7 @@ import string
 import numpy as np
 
 
-def FragmentationKaandorpPartial_FieldDataComp(figure_direc, scenario, shore_time, lambda_frag, density,
+def FragmentationKaandorpPartial_FieldDataComp(figure_direc, scenario, shore_time, lambda_frag_list, density,
                                                fig_size=(10, 14), x_label='Size (mm)',
                                                y_label=r'Normalized Particle Number (n mm$^{-1}$)',
                                                ax_ticklabel_size=12, ax_label_size=14, legend_size=14):
@@ -21,9 +21,11 @@ def FragmentationKaandorpPartial_FieldDataComp(figure_direc, scenario, shore_tim
 
     # Loading the data
     prefix = 'size_distribution'
-    data_dict = vUtils.FragmentationKaandorpPartial_load_data(scenario=scenario, prefix=prefix,
-                                                              data_direc=data_direc, shore_time=shore_time,
-                                                              lambda_frag=lambda_frag, rho=density, postprocess=True)
+    data_dict = {}
+    for lambda_frag in lambda_frag_list:
+        data_dict[lambda_frag] = vUtils.FragmentationKaandorpPartial_load_data(scenario=scenario, prefix=prefix,
+                                                                  data_direc=data_direc, shore_time=shore_time,
+                                                                  lambda_frag=lambda_frag, rho=density, postprocess=True)
     time_index = data_dict['final_index']
     beach_state_list = ['adrift_open_surf', 'adrift_10km_surf', 'beach']
     field_dict = utils.load_obj(vUtils.FragmentationKaandorpPartial_fielddata_filename())
@@ -42,9 +44,11 @@ def FragmentationKaandorpPartial_FieldDataComp(figure_direc, scenario, shore_tim
 
     # Plotting the model distributions
     for ax_index, sub_ax in enumerate(ax):
-        norm_factor = data_dict[beach_state_list[ax_index]][time_index][0]
-        sub_ax.plot(sizes_class, data_dict[beach_state_list[ax_index]][time_index] / norm_factor, linestyle='-',
-                    color='black')
+        for lambda_index, lambda_frag in enumerate(lambda_frag_list):
+            norm_factor = data_dict[lambda_frag][beach_state_list[ax_index]][time_index][0]
+            sub_ax.plot(sizes_class, data_dict[lambda_frag][beach_state_list[ax_index]][time_index] / norm_factor,
+                        linestyle='-',
+                        color=vUtils.discrete_color_from_cmap(index=lambda_index, subdivisions=len(lambda_frag_list)))
 
     # Adding the field data
     # First for the open ocean
@@ -66,7 +70,6 @@ def FragmentationKaandorpPartial_FieldDataComp(figure_direc, scenario, shore_tim
     plt.savefig(file_name, bbox_inches='tight')
 
 
-
 def subfigure_title(index, beach_state):
     """
     setting the title of the subfigure
@@ -76,4 +79,5 @@ def subfigure_title(index, beach_state):
     :return:
     """
     alphabet = string.ascii_lowercase
-    return '({}) {}'.format(alphabet[index], beach_state)
+    title_dict = {'adrift_open_surf': 'Adrift - open ocean', 'adrift_10km_surf': 'Adrift - coastal', 'beach': 'Beach'}
+    return '({}) {}'.format(alphabet[index], title_dict[beach_state])
--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
@@ -10,28 +10,32 @@ import Analysis
 
 def run(scenario, figure_direc: str):
     shore_time = 20
-    lambda_frag_list = np.array([100, 200, 300, 388, 1000, 10000])
+    lambda_frag_list = np.array([388, 1000, 10000, 38000])
     rho = 920
 
     # Standardizing field data
     Analysis.FragmentationKaandorpPartial_fielddata(to_overwrite=False)
-    #
-    # SizeSpectrumTime.FragmentationKaandorpPartial_SizeSpectrumTime(figure_direc=figure_direc, scenario=scenario,
-    #                                                                shore_time=shore_time, lambda_frag_list=lambda_frag_list,
-    #                                                                density=rho)
-    #
+
+    SizeSpectrumTime.FragmentationKaandorpPartial_SizeSpectrumTime(figure_direc=figure_direc, scenario=scenario,
+                                                                   shore_time=shore_time,
+                                                                   lambda_frag_list=lambda_frag_list,
+                                                                   density=rho)
+
     # Animation.FragmentationKaandorpPartial_Animation(scenario=scenario, figure_direc=figure_direc, shore_time=20,
     #                                                  lambda_frag=388, rho=rho, simulation_years=1, ocean_frag=False)
     #
-    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc, shore_time=shore_time,
+    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
+    #                                                    shore_time=shore_time,
     #                                                    lambda_frag=300, rho=rho, simulation_length=1)
-    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc, shore_time=shore_time,
+    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
+    #                                                    shore_time=shore_time,
     #                                                    lambda_frag=388, rho=rho, simulation_length=1)
-    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc, shore_time=shore_time,
+    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
+    #                                                    shore_time=shore_time,
     #                                                    lambda_frag=1000, rho=rho, simulation_length=1)
-    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc, shore_time=shore_time,
+    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
+    #                                                    shore_time=shore_time,
     #                                                    lambda_frag=10000, rho=rho, simulation_length=1)
-    #
     #
     # SizeSpectrumBeach.FragmentationKaandorpPartial_SizeSpectrumBeach(figure_direc=figure_direc, scenario=scenario,
     #                                                                  shore_time=shore_time, lambda_frag=388,
@@ -46,6 +50,7 @@ def run(scenario, figure_direc: str):
     #                                                                  shore_time=shore_time, lambda_frag=1000,
     #                                                                  density=rho)
 
+    lambda_frag_list = np.array([388, 1000, 10000, 38000])
     FieldDataComp.FragmentationKaandorpPartial_FieldDataComp(figure_direc=figure_direc, scenario=scenario,
-                                                             shore_time=shore_time, lambda_frag=388, density=rho)
-
+                                                             shore_time=shore_time, lambda_frag_list=lambda_frag_list,
+                                                             density=rho)
