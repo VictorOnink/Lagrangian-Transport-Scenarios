373dbcd80d04b3b65cd14b51efa5695d80d9b50e
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-09-17 09:41:06 +0200

    update

--- bin/analysis_submission.sh
+++ bin/analysis_submission.sh
@@ -54,7 +54,7 @@ export INPUT
 ADVECTION_DATA=2
 export ADVECTION_DATA
 #Number of years the simulation runs
-SIMLEN=2
+SIMLEN=3
 export SIMLEN
 # For the analysis, if we have multiple years that we want to combine into one analysis set (so if we have continuous
 # particle release), then this how many years we want to include
