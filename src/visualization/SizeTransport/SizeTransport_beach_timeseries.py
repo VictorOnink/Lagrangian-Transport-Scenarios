6886614378dc63a25dfbe259a8bc46a69ddbba17
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-09-20 11:19:21 +0200

    5

--- src/visualization/SizeTransport/SizeTransport_beach_timeseries.py
+++ src/visualization/SizeTransport/SizeTransport_beach_timeseries.py
@@ -19,7 +19,7 @@ def SizeTransport_beach_timeseries(scenario, figure_direc, size_list, rho_list, 
     # Loading in the data
     prefix = 'timeseries'
     timeseries_dict = {}
-    beach_state_list = ['beach', 'adrift', 'seabed', 'total']
+    beach_state_list = ['beach', 'adrift', 'seabed', 'removed']
     if without_seabed:
         _ = beach_state_list.pop(-1)
     for index, size in enumerate(size_list):
@@ -31,7 +31,7 @@ def SizeTransport_beach_timeseries(scenario, figure_direc, size_list, rho_list, 
             for beach_state in beach_state_list:
                 timeseries_dict[size][tau][beach_state] = data_dict[beach_state]
             timeseries_dict[size][tau]['time_raw'] = data_dict['time']
-            timeseries_dict[size][tau]['total'] = data_dict['total'][0]
+            timeseries_dict[size][tau]['total'] = data_dict['total']
 
     # Normalizing all the particle counts with the total number of particles, and then multiplying by 100 to get a
     # percentage
