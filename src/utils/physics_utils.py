d42b8cd38fa425e76e3ddd010faa36fc068714d5
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-09-21 13:59:26 +0200

    5

--- src/utils/physics_utils.py
+++ src/utils/physics_utils.py
@@ -519,6 +519,8 @@ def vertical_reflecting_boundary(particle, fieldset, time):
         particle.depth = local_bathymetry - overshoot
     else:
         particle.depth = particle.potential
+    if particle.depth < fieldset.SURF_Z:
+        print('This is where it goes wrong')
 
 
 def sticky_seafloor_boundary(particle, fieldset, time):
