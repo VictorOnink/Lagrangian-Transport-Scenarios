296f7f580b44394b10c7e88904643f781069349d
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-09-28 14:26:24 +0200

    update

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -44,7 +44,8 @@ def run(scenario, figure_direc: str):
     # SizeTransport_relative_concentrations(scenario=scenario, figure_direc=figure_direc, size_list=size_list,
     #                                       beach_state='beach', time_selection=time_select).plot()
 
-    size_list = np.array([5000, 2500, 1250, 625, 313, 156, 78, 39, 20, 10, 5, 2]) * settings.SIZE_FACTOR
+    # size_list = np.array([5000, 2500, 1250, 625, 313, 156, 78, 39, 20, 10, 5, 2]) * settings.SIZE_FACTOR
+    size_list = np.array([5000]) * settings.SIZE_FACTOR
     # Creating figures of the timeseries of the number of particles that are beached/adrift/seabed
     # SizeTransport_beach_timeseries(scenario=scenario, figure_direc=figure_direc, size_list=size_list,
     #                                simulation_years=3).plot()
