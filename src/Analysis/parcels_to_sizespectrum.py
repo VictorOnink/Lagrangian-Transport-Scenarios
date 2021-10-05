b3d02277dbea8867dde9aa655faf5c21a45c4592
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-10-05 14:40:17 +0200

    update

--- src/Analysis/parcels_to_sizespectrum.py
+++ src/Analysis/parcels_to_sizespectrum.py
@@ -75,8 +75,8 @@ class parcels_to_sizespectrum:
                                     for weight in self.weight_list:
                                         for index_time in range(0, self.time_list.__len__(), self.time_analysis_step):
                                             self.output_dict[reservoir][weight][index_time] += dataset_post[reservoir][weight][index_time]
-                            # else:
-                            #     utils.print_statement('The file {} does not exist'.format(file_name), to_print=True)
+                            else:
+                                utils.print_statement('The file {} does not exist'.format(file_name), to_print=True)
             # Adding the index of the final timestep for ease later on
             self.output_dict['final_index'] = dataset_post['final_index']
             # Saving everything
