29b8e15b0f623e05561d6cf3c2c2f0ae22242e8f
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-10-06 13:38:43 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Concentration.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Concentration.py
@@ -125,7 +125,7 @@ def set_normalization(beach_state):
     :return:
     """
     if beach_state == 'adrift':
-        vmin, vmax = 1e6, 1e16
+        vmin, vmax = 1e6, 1e15
     elif beach_state == 'beach':
         vmin, vmax = 1, 1e5
     return colors.LogNorm(vmin=vmin, vmax=vmax)
