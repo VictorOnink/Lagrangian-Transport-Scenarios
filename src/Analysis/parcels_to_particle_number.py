299ecf32bbe81894c4af0d79127f692a5bb01946
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-10-11 16:26:17 +0200

    update

--- src/Analysis/parcels_to_particle_number.py
+++ src/Analysis/parcels_to_particle_number.py
@@ -91,6 +91,7 @@ class parcels_to_particle_number:
                         for index_id in range(1, c_id.size):
                             new_particle_mass = utils.mass_per_size_class(k=index_id, f=self.f)
                             if p_id == 55:
+                                print('c_id={}'.format(c_id))
                                 print('id = {}-{}'.format(p_id, index_id))
                                 print('mass fraction = {}'.format(new_particle_mass))
                                 print('t_ind = {}'.format(t_ind))
