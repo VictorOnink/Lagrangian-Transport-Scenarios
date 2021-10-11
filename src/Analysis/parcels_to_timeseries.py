d79fa2341f7645e963c1900ae0a9aba622c29d6f
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-10-11 15:33:24 +0200

    update

--- src/Analysis/parcels_to_timeseries.py
+++ src/Analysis/parcels_to_timeseries.py
@@ -66,6 +66,7 @@ class parcels_to_timeseries:
                                 self.output_dict['total'][size_class][weight] += self.output_dict[beach_state][size_class][weight]
                     else:
                         self.output_dict['total'] += self.output_dict[beach_state]
+                print(self.output_dict['total'])
                 # Saving the output
                 utils.save_obj(filename=output_name, item=self.output_dict)
                 str_format = settings.STARTYEAR, settings.STARTMONTH, settings.RUN, settings.RESTART
