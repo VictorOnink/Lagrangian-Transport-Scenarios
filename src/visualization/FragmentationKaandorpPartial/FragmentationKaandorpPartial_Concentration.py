033b3e137567dcf7e46571c364793473ab7f029f
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-10-14 17:04:58 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Concentration.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Concentration.py
@@ -126,7 +126,7 @@ def set_normalization(beach_state, simulation_year):
     """
     if beach_state == 'adrift':
         if simulation_year == 0:
-            vmin, vmax = 1e6, 1e15
+            vmin, vmax = 1e0, 1e7
         elif simulation_year == 1:
             vmin, vmax = 1e15, 1e24
     elif beach_state == 'beach':
