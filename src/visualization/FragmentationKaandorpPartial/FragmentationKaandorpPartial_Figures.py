ff18aaade1a3ddc11e90823b427259a0c4330101
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-10-14 16:28:19 +0200

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
@@ -27,9 +27,9 @@ def run(scenario, figure_direc: str):
     # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
     #                                                    shore_time=shore_time, lambda_frag=300, rho=rho,
     #                                                    simulation_length=1, weight='particle_number').plot()
-    timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
-                                                       shore_time=shore_time, lambda_frag=388, rho=rho,
-                                                       simulation_length=2, weight='particle_mass').plot()
+    # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
+    #                                                    shore_time=shore_time, lambda_frag=388, rho=rho,
+    #                                                    simulation_length=2, weight='particle_mass').plot()
     # timeseries.FragmentationKaandorpPartial_timeseries(scenario=scenario, figure_direc=figure_direc,
     #                                                    shore_time=shore_time, lambda_frag=1000, rho=rho,
     #                                                    simulation_length=1, weight='particle_number').plot()
@@ -37,22 +37,22 @@ def run(scenario, figure_direc: str):
     #                                                    shore_time=shore_time, lambda_frag=10000, rho=rho,
     #                                                    simulation_length=1, weight='particle_number').plot()
 
-    FieldDataComp.FragmentationKaandorpPartial_FieldDataComp(figure_direc=figure_direc, scenario=scenario,
-                                                             shore_time=shore_time, lambda_frag_list=lambda_frag_list,
-                                                             rho=rho, sink=False).plot()
-    FieldDataComp.FragmentationKaandorpPartial_FieldDataComp(figure_direc=figure_direc, scenario=scenario,
-                                                             shore_time=shore_time, lambda_frag_list=lambda_frag_list,
-                                                             rho=rho, sink=True).plot()
-    box_model.FragmentationKaandorpPartial_boxmodelcomparison(figure_direc=figure_direc, scenario=scenario,
-                                                              shore_time=shore_time, lambda_frag=388,
-                                                              rho=rho, sink=False, sim_length=2).plot()
-    box_model.FragmentationKaandorpPartial_boxmodelcomparison(figure_direc=figure_direc, scenario=scenario,
-                                                              shore_time=shore_time, lambda_frag=388,
-                                                              rho=rho, sink=True, sim_length=2).plot()
+    # FieldDataComp.FragmentationKaandorpPartial_FieldDataComp(figure_direc=figure_direc, scenario=scenario,
+    #                                                          shore_time=shore_time, lambda_frag_list=lambda_frag_list,
+    #                                                          rho=rho, sink=False).plot()
+    # FieldDataComp.FragmentationKaandorpPartial_FieldDataComp(figure_direc=figure_direc, scenario=scenario,
+    #                                                          shore_time=shore_time, lambda_frag_list=lambda_frag_list,
+    #                                                          rho=rho, sink=True).plot()
+    # box_model.FragmentationKaandorpPartial_boxmodelcomparison(figure_direc=figure_direc, scenario=scenario,
+    #                                                           shore_time=shore_time, lambda_frag=388,
+    #                                                           rho=rho, sink=False, sim_length=2).plot()
+    # box_model.FragmentationKaandorpPartial_boxmodelcomparison(figure_direc=figure_direc, scenario=scenario,
+    #                                                           shore_time=shore_time, lambda_frag=388,
+    #                                                           rho=rho, sink=True, sim_length=2).plot()
 
-    vertical_profile.FragmentationKaandorpPartial_vertical_profile(figure_direc=figure_direc, scenario=scenario,
-                                                                   shore_time=shore_time, lambda_frag=388, rho=rho,
-                                                                   simulation_year=2, weight='particle_number').plot()
+    # vertical_profile.FragmentationKaandorpPartial_vertical_profile(figure_direc=figure_direc, scenario=scenario,
+    #                                                                shore_time=shore_time, lambda_frag=388, rho=rho,
+    #                                                                simulation_year=2, weight='particle_number').plot()
     for beach_state in ['adrift', 'beach']:
         for year in [0, 1]:
             concentration.FragmentationKaandorpPartial_Concentration(scenario=scenario, figure_direc=figure_direc,
