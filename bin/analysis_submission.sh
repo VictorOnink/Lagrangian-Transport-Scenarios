2faa361dac8115d1cc71ab79d8ef7fb8277ade35
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-10-27 16:14:40 +0200

    update

--- bin/analysis_submission.sh
+++ bin/analysis_submission.sh
@@ -219,8 +219,8 @@ for SHORETIME in "${SHORETIME_list[@]}"; do
                   done
 
                   # submitting the job
-#                  jobid=$(sbatch --parsable jobsubmissionFile_${RUN}_${RESTARTNUM}.sh)
-#                  echo -n ':'${jobid} >> job_id.txt
+                  jobid=$(sbatch --parsable jobsubmissionFile_${RUN}_${RESTARTNUM}.sh)
+                  echo -n ':'${jobid} >> job_id.txt
 #                  # deleting the submission file
                   rm jobsubmissionFile_${RUN}_${RESTARTNUM}.sh
                 done
@@ -267,8 +267,8 @@ for SHORETIME in "${SHORETIME_list[@]}"; do
 
           # Submitting the job that will join all the various analysis files together
           dependence=$( cat job_id.txt )
-#          sbatch --dependency=afterok${dependence} jobsubmissionFile.sh
-          sbatch jobsubmissionFile.sh
+          sbatch --dependency=afterok${dependence} jobsubmissionFile.sh
+#          sbatch jobsubmissionFile.sh
           rm jobsubmissionFile.sh
           rm job_id.txt
         done
