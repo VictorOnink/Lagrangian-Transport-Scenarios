16fdfb75d91467347f1f9d4e243372e5f1890d24
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-11-04 15:19:47 +0100

    update

--- bin/simulation_submission.sh
+++ bin/simulation_submission.sh
@@ -47,7 +47,7 @@ export OCEAN_FRAG
 POST_PROCESS=0
 export POST_PROCESS
 #the starting year of the simulation, and how many years the simulation will take
-STARTYEAR=2010
+STARTYEAR=2012
 STARTMONTH_list=(1 2 3 4 5 6 7 8 9 10 11 12)
 STARTDAY=1
 export STARTYEAR
