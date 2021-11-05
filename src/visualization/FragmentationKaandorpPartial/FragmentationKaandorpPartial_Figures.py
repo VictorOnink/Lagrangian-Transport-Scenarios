b620d3f6846da925c82782a90cde5e6be218a29d
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-11-05 11:42:22 +0100

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_Figures.py
@@ -35,7 +35,8 @@ def run(scenario, figure_direc: str):
     #
     FieldDataComp.FragmentationKaandorpPartial_FieldDataComp(figure_direc=figure_direc, scenario=scenario,
                                                              shore_time=shore_time, lambda_frag_list=lambda_frag_list,
-                                                             rho=rho).plot()
+                                                             rho=rho, input_list=['LebretonDivision',
+                                                                                  'LebretonKaandorpInit']).plot()
     #
     # box_model.FragmentationKaandorpPartial_boxmodelcomparison(figure_direc=figure_direc, scenario=scenario,
     #                                                           shore_time=shore_time, lambda_frag=388,
