e52f3934fe1820d108f60d30f11a04f5fed33a21
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2021-11-18 17:16:29 +0100

    update

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
@@ -39,7 +39,7 @@ class FragmentationKaandorpPartial_FieldDataComp:
         self.ax_label_size = 14
         self.legend_size = 10
         self.xmin, self.xmax = 1e-1, 2e2
-        self.ymin, self.ymax = 1e-4, 1e5
+        self.ymin, self.ymax = 1e-4, 1e4
         self.ax_range = self.xmax, self.xmin, self.ymax, self.ymin
         self.twin_ymin, self.twin_ymax = 1e-5, 1e1
         self.twin_ax_range = self.xmax, self.xmin, self.twin_ymax, self.twin_ymin
