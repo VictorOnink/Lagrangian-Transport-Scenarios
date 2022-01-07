b1402983481235bb9a2e959389f7fd9db82da117
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-01-07 15:45:22 +0100

    new simulations!

--- bin/simulation_submission.sh
+++ bin/simulation_submission.sh
@@ -51,7 +51,7 @@ export OCEAN_FRAG
 POST_PROCESS=1
 export POST_PROCESS
 #the starting year of the simulation, and how many years the simulation will take
-STARTYEAR=2011
+STARTYEAR=2012
 STARTMONTH_list=(1 2 3 4 5 6 7 8 9 10 11 12)
 STARTDAY=1
 export STARTYEAR
@@ -67,7 +67,7 @@ export ADVECTION_DATA
 #Start year of the simulation. 0 = new simulation, otherwise it picks up from a previous simulation
 START=0
 #Number of years the simulation runs
-SIMLEN=2
+SIMLEN=1
 export SIMLEN
 #Inclusion of Stokes drift. 0 = include stokes, 1 = do not include stokes
 STOKES=0
