174cc74b31b69cfdf605ea9da5eae3ae07fdfb07
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-01-11 14:15:50 +0100

    figures!

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
@@ -185,7 +185,7 @@ class FragmentationKaandorpPartial_FieldDataComp:
         print(number_inter)
         number_norm = number_inter / utils.size_range(units='mm', size_class_number=settings.SIZE_CLASS_NUMBER)
         print(number_norm)
-        number_norm /= number_inter[0]
+        number_norm = number_norm / number_inter[0]
         print(number_norm)
         print(utils.size_range(units='mm', size_class_number=settings.SIZE_CLASS_NUMBER))
         # Plotting the normalized count and mass inputs
