8b157692814ca84fb559320adc1c86aa085f0885
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-01-11 14:10:21 +0100

    figures!

--- src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
+++ src/visualization/FragmentationKaandorpPartial/FragmentationKaandorpPartial_FieldDataComp.py
@@ -184,23 +184,12 @@ class FragmentationKaandorpPartial_FieldDataComp:
         number_inter = interpolation_function(utils.size_range(units='mm', size_class_number=settings.SIZE_CLASS_NUMBER))
         number_norm = number_inter / utils.size_range(units='mm', size_class_number=settings.SIZE_CLASS_NUMBER)
         number_norm /= number_inter[0]
-        # Converting the particle number to the particle mass
-        mass_inter = np.zeros(number_inter.shape, dtype=float)
-        for size_class in range(mass_inter.size):
-            mass_inter[size_class] = number_inter[size_class] / (2 ** (settings.DN * size_class))
-        # Normalize by the particle mass in size class k = 0
-        mass_inter /= mass_inter[0]
+        print(number_norm)
         # Plotting the normalized count and mass inputs
-        norm_factor = utils.size_range(units='mm', size_class_number=settings.SIZE_CLASS_NUMBER)
         ax[2].plot(utils.size_range(units='mm', size_class_number=settings.SIZE_CLASS_NUMBER),
                    number_norm, marker=self.field_marker, linestyle=self.field_line, color='tab:cyan',
                    label=r'Zeri et al. (2021) Input')
         ax[2].legend(fontsize=self.legend_size, loc=self.legend_loc)
-        ax[3].plot(utils.size_range(units='mm', size_class_number=settings.SIZE_CLASS_NUMBER),
-                   mass_inter / norm_factor, marker=self.field_marker, linestyle=self.field_line, color='tab:cyan',
-                   label=r'Zeri et al. (2021) Input')
-        ax[3].legend(fontsize=self.legend_size, loc=self.legend_loc)
-
 
 
 def subfigure_title(index, beach_state):
