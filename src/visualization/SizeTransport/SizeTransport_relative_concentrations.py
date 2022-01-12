297911ce6a97150f2d873a5512b28d15d3e66bfa
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-01-12 11:25:00 +0100

    figures!

--- src/visualization/SizeTransport/SizeTransport_relative_concentrations.py
+++ src/visualization/SizeTransport/SizeTransport_relative_concentrations.py
@@ -98,7 +98,9 @@ class SizeTransport_relative_concentrations:
             if self.beach_state in ['adrift']:
                 # ax_list[index].pcolormesh(Lon, Lat, concentration_dict['adrift'][index], norm=norm, cmap=self.cmap,
                 #                           zorder=200)
-                ax_list[index].scatter(Lon.flatten(), Lat.flatten(), c=concentration_dict['beach'][index].flatten(),
+                not_nan = ~np.isnan(concentration_dict['beach'][index].flatten())
+                ax_list[index].scatter(Lon.flatten()[not_nan], Lat.flatten()[not_nan],
+                                       c=concentration_dict['beach'][index].flatten()[not_nan],
                                        norm=norm, cmap=self.cmap, zorder=200, s=6)
             else:
                 ax_list[index].scatter(Lon.flatten(), Lat.flatten(), c=concentration_dict['beach'][index].flatten(),
