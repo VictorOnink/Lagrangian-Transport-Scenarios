26169b1532154b8f86aa92c40fb72a1133276ae3
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-01-12 11:23:11 +0100

    figures!

--- src/visualization/SizeTransport/SizeTransport_relative_concentrations.py
+++ src/visualization/SizeTransport/SizeTransport_relative_concentrations.py
@@ -96,10 +96,10 @@ class SizeTransport_relative_concentrations:
         # The actual plotting of the figures
         for index, size in enumerate(self.size_list):
             if self.beach_state in ['adrift']:
-                ax_list[index].pcolormesh(Lon, Lat, concentration_dict['adrift'][index], norm=norm, cmap=self.cmap,
-                                          zorder=200)
+                # ax_list[index].pcolormesh(Lon, Lat, concentration_dict['adrift'][index], norm=norm, cmap=self.cmap,
+                #                           zorder=200)
                 ax_list[index].scatter(Lon.flatten(), Lat.flatten(), c=concentration_dict['beach'][index].flatten(),
-                                       norm=norm, cmap=self.cmap, zorder=200, s=6, edgecolors='white')
+                                       norm=norm, cmap=self.cmap, zorder=200, s=6)
             else:
                 ax_list[index].scatter(Lon.flatten(), Lat.flatten(), c=concentration_dict['beach'][index].flatten(),
                                        norm=norm, cmap=self.cmap, zorder=200)
