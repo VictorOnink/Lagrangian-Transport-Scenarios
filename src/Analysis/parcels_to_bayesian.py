191b43aa3215bd0e81bfdf6371c623ab9c57fca6
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-01-26 18:02:13 +0100

    figures!

--- src/Analysis/parcels_to_bayesian.py
+++ src/Analysis/parcels_to_bayesian.py
@@ -28,11 +28,12 @@ class parcels_to_bayesian:
     def source_cluster(self):
         # Load the starting locations of all of the particles
         parcels_dataset = xr.load_dataset(self.file_dict[settings.RUN][0])
+        p_lat = parcels_dataset.lat[:, 0]
+        p_lon = parcels_dataset.lon[:, 0]
 
         # Group by the starting location and determine the number of particles released at each site
-        df = pd.DataFrame.from_dict({'particle_release_lon': parcels_dataset.lon[:, 0],
-                                     'particle_release_lat': parcels_dataset.lat[:, 0],
-                                     'weight': np.ones(parcels_dataset.lat[:, 0].shape)})
+        df = pd.DataFrame.from_dict({'particle_release_lon': p_lon, 'particle_release_lat': p_lat,
+                                     'weight': np.ones(p_lat.shape)})
         release_sites = df.groupby(['particle_release_lon', 'particle_release_lat']).count()
 
         # Get the cluster lon, lat and weight
@@ -50,8 +51,21 @@ class parcels_to_bayesian:
         # Now, we create an array that for each particle (not each release site, but each particle) will indicate the
         # release cluster from which it originates
         release_cluster = np.zeros(parcels_dataset.lon[:, 0].shape)
-        print(release_cluster.shape)
-
+        cluster_index = 0
+        cluster_dict = {}
+        for index_lon in range(0, cluster_lon.size - 1):
+            for index_lat in range(0, cluster_lat.size - 1):
+                lat_min, lat_max = cluster_lat[index_lat], cluster_lat[index_lat + 1]
+                lon_min, lon_max = cluster_lon[index_lat], cluster_lon[index_lat + 1]
+                selection = ((lat_min <= p_lat) & (p_lat < lat_max) & (lon_min <= p_lon) & (p_lon < lon_max))
+                if np.nanmax(selection) == 1:
+                    release_cluster[selection] = cluster_index
+                    # Within the cluster dict we save the mid lon/lat for each cluster, as well as the number of
+                    # particles
+                    cluster_dict[cluster_index] = ((lat_min + lat_max) / 2, (lon_min + lon_max) / 2, np.nanmax(selection))
+                    cluster_index += 1
+        print(release_cluster)
+        print(cluster_index)
 
 
     def cluster_lon_lat(self):
