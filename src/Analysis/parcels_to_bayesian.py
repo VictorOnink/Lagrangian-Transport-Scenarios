cb972627b702dbad8e6e4a5f284ed9bb069f4f8b
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-01-27 10:00:47 +0100

    (figures!)

--- src/Analysis/parcels_to_bayesian.py
+++ src/Analysis/parcels_to_bayesian.py
@@ -57,7 +57,7 @@ class parcels_to_bayesian:
             for index_lat in range(0, cluster_lat.size - 1):
                 lat_min, lat_max = cluster_lat[index_lat], cluster_lat[index_lat + 1]
                 lon_min, lon_max = cluster_lon[index_lat], cluster_lon[index_lat + 1]
-                selection = np.logical_and(lat_min <= p_lat, p_lat < lat_max) & np.logical_and(lon_min <= p_lon, p_lon < lon_max)
+                selection = np.logical_and(np.logical_and(lat_min <= p_lat, p_lat < lat_max), np.logical_and(lon_min <= p_lon, p_lon < lon_max))
                 if np.nanmax(selection) == 1:
                     release_cluster[selection] = cluster_index
                     # Within the cluster dict we save the mid lon/lat for each cluster, as well as the number of
@@ -65,7 +65,7 @@ class parcels_to_bayesian:
                     cluster_dict[cluster_index] = ((lat_min + lat_max) / 2, (lon_min + lon_max) / 2, np.nanmax(selection))
                     cluster_index += 1
                     print(np.unique(release_cluster))
-        print(cluster_dict.keys())
+        print(cluster_dict)
 
 
     def cluster_lon_lat(self):
