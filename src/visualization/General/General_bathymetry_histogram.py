0a5eb9a153b0364d412c7231c10b4e65692f48bd
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-02-15 16:36:08 +0100

    animation

--- src/visualization/General/General_bathymetry_histogram.py
+++ src/visualization/General/General_bathymetry_histogram.py
@@ -29,8 +29,9 @@ class General_bathymetry_histogram:
         dataset = Dataset(self.scenario.file_dict['BATH_filenames'])
         depth = dataset.variables[self.scenario.file_dict['BATH_variables']['DEPTH']][:]
         depth[depth <= 0] = np.nan
-        print(dataset.variable[self.scenario.file_dict['BATH_variables']['LON']][:])
-        print(dataset.variable[self.scenario.file_dict['BATH_variables']['LAT']][:])
+
+        print(dataset.variable['longitude'][:])
+        print(dataset.variable['latitude'][:])
         print(Dataset(self.scenario.file_dict['DISTANCE_filename']).variables['lon'][:])
         print(Dataset(self.scenario.file_dict['DISTANCE_filename']).variables['lat'][:])
 
