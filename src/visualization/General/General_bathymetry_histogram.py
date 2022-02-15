d62f3657c3a93cc7f87476828b4ac0f695bac904
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-02-15 15:18:21 +0100

    animation

--- src/visualization/General/General_bathymetry_histogram.py
+++ src/visualization/General/General_bathymetry_histogram.py
@@ -49,7 +49,7 @@ class General_bathymetry_histogram:
         histogram_depths, _ = np.histogram(depth, bins=depth_bins)
 
         # Normalize all the depth bins by the total number of cells
-        histogram_depths = np.divide(histogram_depths, depth.size)
+        histogram_depths = np.divide(histogram_depths, np.nansum(histogram_depths))
         histogram_depths *= 100
 
         # Creating the figure
