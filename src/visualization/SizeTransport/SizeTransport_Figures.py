f286760d9e92cce9d451f748be01e6fd2f20c989
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-02-16 15:44:10 +0100

    animation

--- src/visualization/SizeTransport/SizeTransport_Figures.py
+++ src/visualization/SizeTransport/SizeTransport_Figures.py
@@ -147,6 +147,7 @@ def run(scenario, figure_direc: str):
     # SizeTransport_CumulativeDistance(figure_direc=figure_direc, scenario=scenario, size_list=size_list).plot()
 
     # Plotting the month average vertical profile
+    size_list = np.array([5000, 2500, 1250, 625, 313, 156, 78, 39, 20, 10, 5, 2]) * settings.SIZE_FACTOR
     for rho in [[30, 920, 980, 1020]]:
         SizeTransport_VerticalProfile(scenario=scenario, figure_direc=figure_direc, size_list=size_list,
                                       time_selection=0, rho_list=rho).plot()
