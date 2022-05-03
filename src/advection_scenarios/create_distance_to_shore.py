bb3c2335cea4d9898b187eddec2c273f2966156e
Victor Onink <31734765+VictorOnink@users.noreply.github.com>
2022-05-03 11:47:06 +0200

    Restructuring

--- src/advection_scenarios/create_distance_to_shore.py
+++ src/advection_scenarios/create_distance_to_shore.py
@@ -81,10 +81,10 @@ def create_distance_to_shore_land(output_name: str, grid: np.array, lon: np.arra
     # Getting the dimensions of the model grid
     # utils.print_statement("grid shape {} {}".format(grid.shape[0], grid.shape[1]))
     mask = grid.mask
-    n_lat, n_lon = mask.shape
+    n_lat, n_lon = grid.shape
     # Initializing the distance array
-    distance = np.zeros(mask.shape)
-    print("distance shape {}".format(distance.shape[0], distance.shape[1]))
+    distance = np.zeros(grid.shape)
+    print("distance shape {} {}".format(distance.shape[0], distance.shape[1]))
     # Creating a memory variable that keeps track of how many cells away land is. That way, when we move from one
     # cell to it's neighbor, then we don't need to check everything again...
     memory_var = 1
